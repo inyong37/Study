19c99c060706bbbfcfe3785fe38739c0b08deb52
Inyong Hwang <inyong1020@gmail.com>
2019-07-23 23:38:42 +0900

    Add files via upload

---
+++ Deep Learning/TENSORFLOW/TF_LSTM.py
@@ -0,0 +1,75 @@
+# Date : 2019-03-12-Tue
+# Model: LSTM (TensorFlow, Python)
+# Data : MNIST
+
+from __future__ import print_function
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+# Training Parameters
+learning_rate = 0.001
+training_steps = 10000
+batch_size = 128
+display_step = 200
+
+# Network Parameters
+num_input = 28
+timesteps = 28
+num_hidden = 128
+num_classes = 10
+
+# tf Graph input
+X = tf.placeholder("float", [None, timesteps, num_input])
+Y = tf.placeholder("float", [None, num_classes])
+
+# Define weights
+weights = {
+    'out': tf.Variable(tf.random_normal([num_hidden, num_classes]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
+
+
+def RNN(x, weights, biases):
+    x = tf.unstack(x, timesteps, 1)
+    lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+    outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+
+logits = RNN(X, weights, biases)
+prediction = tf.nn.softmax(logits)
+
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for step in range(1, training_steps+1):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        batch_x = batch_x.reshape((batch_size, timesteps, num_input))
+        sess.run(train_op, feed_dict={X: batch_x, Y:batch_y})
+        if step % display_step == 0 or step == 1:
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x, Y: batch_y})
+            print("Step " + str(step) + ", Minibatch Loss= " + \
+                  "{:.4f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.3f}".format(acc))
+
+    print("Optimization Finished!")
+
+    test_len = 128
+    test_data = mnist.test.images[:test_len].reshape((-1, timesteps, num_input))
+    test_label = mnist.test.labels[:test_len]
+    print("Testing Accuracy:", \
+          sess.run(accuracy, feed_dict={X: test_data, Y: test_label}))
