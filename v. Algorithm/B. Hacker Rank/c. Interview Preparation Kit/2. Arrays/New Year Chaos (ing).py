16ae2af570e1a500627f74b9faac3fea1eb2e7c1
Inyong Hwang <inyong1020@gmail.com>
2019-08-19 23:55:26 +0900

    Create New Year Chaos (ing).py

---
+++ v. Algorithm/B. Hacker Rank/c. Interview Preparation Kit/2. Arrays/New Year Chaos (ing).py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumBribes function below.
+def minimumBribes(n, q):
+    if n == 2:
+        
+    else:
+        result = 'Too chaotic'
+    return result
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(n, q)
