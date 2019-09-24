7de8f8a1219e325556cfd815efd42af26c1e5208
Inyong Hwang <inyong1020@gmail.com>
2019-09-24 14:27:05 +0900

    Update Lesson3_2_PermMissingElem.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson3_2_PermMissingElem.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson3_2_PermMissingElem.py
@@ -1,4 +1,4 @@
-# https://app.codility.com/demo/results/trainingSEWFXA-6ET/
+# 50% https://app.codility.com/demo/results/trainingSEWFXA-6ET/
 
 def solution(A):
     A = sorted(A)
@@ -7,3 +7,25 @@ def solution(A):
             return A[i]+1
         else:
             pass
+
+# 70% https://app.codility.com/demo/results/trainingPRKTGF-58D/
+
+def solution(A):
+    A = sorted(A)
+    
+    if len(A) == 0:
+        return 1
+    elif len(A) == 1:
+        if A[0] == 1:
+            return 2
+        elif A[0] == 2:
+            return 1
+    elif A[-1] != len(A) + 1:
+        return len(A) + 1
+    else:
+        for i in range(0, len(A)):
+            if A[i]+1 != A[i+1]:
+                return A[i] + 1
+            else:
+                pass
+    return A[-1] + 1
