60ca92510ae19e422ea59bb609c101bae382eddc
Inyong Hwang <inyong1020@gmail.com>
2019-09-24 15:50:17 +0900

    Create Lesson4_2_FrogRiverOne.py

---
+++ v. Algorithm/C. Codility/a. Lessons/Lesson4_2_FrogRiverOne.py
@@ -0,0 +1,15 @@
+# 53% https://app.codility.com/demo/results/trainingP7F727-5EB/
+
+def solution(X, A):
+    check = int(X*(X+1)/2)
+    walk = []
+    for index, item in enumerate(A):
+        if item in walk:
+            pass    
+        else:
+            walk.append(item)
+            if sum(walk) == check:
+                return index
+            else:
+                pass
+    return -1
