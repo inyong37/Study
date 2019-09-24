580292767fb3f2da4aebd07b01688076a0c63ffd
Inyong Hwang <inyong1020@gmail.com>
2019-09-24 22:28:42 +0900

    Update Lesson4_2_FrogRiverOne.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson4_2_FrogRiverOne.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson4_2_FrogRiverOne.py
@@ -13,3 +13,16 @@ def solution(X, A):
             else:
                 pass
     return -1
+
+# 100% https://app.codility.com/demo/results/trainingYRFX8E-VJ9/
+
+def solution(X, A):
+    check = [0] * X
+    count = 0
+    for i in range(0, len(A)):
+        if check[A[i]-1] == 0:
+            check[A[i]-1] = 1
+            count += 1
+            if count == X:
+                return i
+    return -1
