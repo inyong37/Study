fa70387a4ca17774c3aa8e605ae797e0ec43f34e
Inyong Hwang <inyong1020@gmail.com>
2019-09-24 15:38:15 +0900

    Update Lesson4_1_PermCheck.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson4_1_PermCheck.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson4_1_PermCheck.py
@@ -6,3 +6,14 @@ def solution(A):
         return 0
     else:
         return 1
+
+# 100% https://app.codility.com/demo/results/trainingM9DE6F-9XS/
+
+def solution(A):
+    A = sorted(A)
+    for i in range(0, len(A)):
+        if A[i] == i + 1:
+            pass
+        else:
+            return 0
+    return 1
