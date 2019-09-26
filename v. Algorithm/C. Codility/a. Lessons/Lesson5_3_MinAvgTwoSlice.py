610894f0aab4743229d778c91653306f3d4e6330
Inyong Hwang <inyong1020@gmail.com>
2019-09-26 18:05:36 +0900

    Update Lesson5_3_MinAvgTwoSlice.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson5_3_MinAvgTwoSlice.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson5_3_MinAvgTwoSlice.py
@@ -12,3 +12,21 @@ def solution(A):
             result.append(p)
     index, item = min(enumerate(value), key=operator.itemgetter(1))
     return result[index]
+
+# 60% https://app.codility.com/demo/results/trainingX5UR8F-42V/ 
+
+def solution(A):
+    result = 0
+    value = 100001
+    
+    for i in range(0, len(A)-1):
+        if (A[i] + A[i+1])/2 < value:
+            result = i
+            value = (A[i] + A[i+1])/2
+
+    for i in range(0, len(A)-2):
+        if result < len(A)-2 and (A[i] + A[i+1] + A[i+2])/3 < value:
+            result = i
+            value = (A[i] + A[i+1] + A[i+2])/3 < value
+    
+    return result
