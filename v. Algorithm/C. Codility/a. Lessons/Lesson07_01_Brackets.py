ccb5a573754ea974627548fec6e0aff27b0c5715
Inyong Hwang <inyong1020@gmail.com>
2019-10-11 01:31:40 +0900

    Update Lesson07_01_Brackets.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson07_01_Brackets.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson07_01_Brackets.py
@@ -37,6 +37,40 @@ def solution(S):
                     result_c -= 1
             
             if result_a < 0 or result_b < 0 or result_c < 0:
+
+# 100% https://app.codility.com/demo/results/trainingURVSB3-7PZ/
+
+def solution(S):
+    if len(S) == 0:
+        return 1
+    elif len(S) % 2 != 0:
+        return 0
+    else:
+        count = 0
+        stack = []
+        for i in S:
+            if i == '{' or i == '[' or i =='(':
+                # stack.push()
+                stack.append(i)
+            # elif i == '}' or i ==']' or i ==')':
+            else:
+                if len(stack) == 0:
+                    return 0
+                else:
+                    bracket = stack.pop()
+                    if bracket == '{':
+                        if i != '}':
+                            return 0
+                    elif bracket == '[':
+                        if i != ']':
+                            return 0
+                    else:
+                        if i != ')':
+                            return 0
+        if len(stack) > 0:
+            return 0
+        return 1
+            
                 return 0
             
             # print(result_a, result_b, result_c)
