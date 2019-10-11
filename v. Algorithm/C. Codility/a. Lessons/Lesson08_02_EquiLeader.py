788f89a50460da7d98274af2393643ec7bacb3bf
Inyong Hwang <inyong1020@gmail.com>
2019-10-12 01:50:17 +0900

    Update Lesson08_02_EquiLeader.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson08_02_EquiLeader.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson08_02_EquiLeader.py
@@ -44,3 +44,49 @@ def solution(A):
     
     return result
 
+# 100% https://app.codility.com/demo/results/training96ZPJ2-YVE/
+
+def solution(A):
+    # find leader
+    ele = ''
+    cnt = 0
+    for val in A:
+        # ele init
+        if ele == '':
+            ele = val
+            cnt = 1
+        else:
+            if val != ele:
+                cnt -= 1
+                # ele init
+                if cnt == 0:
+                    ele = ''
+            else:
+                cnt += 1
+    
+    # if ele is none
+    if cnt == 0:
+        return 0
+    
+    # find i (last index)
+    cnt = 0
+    i = 0
+    for idx, val in enumerate(A):
+        if val == ele:
+            cnt += 1
+            i = idx
+    
+    # ?
+    if cnt < len(A)//2:
+        return 0
+    
+    result = 0
+    left = 0
+    for idx, val in enumerate(A):
+        if val == ele:
+            left += 1
+        if left > (idx + 1)//2  and cnt - left > (len(A) - idx - 1)//2:
+            result += 1
+    
+    return result
+
