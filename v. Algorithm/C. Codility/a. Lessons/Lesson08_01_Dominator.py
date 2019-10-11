bb143c1a191a3add9d4bb28d2362c26d1fb3f767
Inyong Hwang <inyong1020@gmail.com>
2019-10-11 20:05:39 +0900

    Update Lesson08_01_Dominator.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson08_01_Dominator.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson08_01_Dominator.py
@@ -20,3 +20,38 @@ def solution(A):
                 return index
         # return indexes
 
+# 100% https://app.codility.com/demo/results/trainingU9F45X-KWV/
+
+def solution(A):
+    ele = ''
+    cnt = 0
+    # pick ele
+    for value in A:
+        # ele init
+        if ele == '':
+            ele = value
+            cnt = 1
+        else:
+            if value != ele:
+                cnt -= 1
+                # ele init
+                if cnt == 0:
+                    ele = ''
+            else:
+                cnt += 1
+    if cnt == 0:
+        return -1
+    
+    # collect idx
+    cnt = 0
+    idx = 0
+    for i, value in enumerate(A):
+        if value == ele:
+            cnt += 1
+            idx = i
+    
+    if cnt > len(A)//2:
+        # print(idx)
+        return idx
+    
+    return -1
