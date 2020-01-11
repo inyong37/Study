4b450c474ff7ff4375f609e86ec27509c004906f
Inyong Hwang <inyong1020@gmail.com>
2020-01-11 20:23:28 +0900

    Update Lesson10_01_MinPerimeterRectangle.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson10_01_MinPerimeterRectangle.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson10_01_MinPerimeterRectangle.py
@@ -18,3 +18,13 @@ def solution(N):
             if 2 * (i + a) < answer:
                 answer = 2 * (i + a)
     return answer
+
+# 100% https://app.codility.com/demo/results/trainingHG4UJW-AZS/
+
+from math import sqrt
+
+def solution(N):
+    for i in range(int(sqrt(N)), 0, -1):
+        if N % i == 0:
+            return int(2 * (N/i + i))
+    
