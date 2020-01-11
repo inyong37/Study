313a77a0aaf172f186b0cf4179541c453e87915f
Inyong Hwang <inyong1020@gmail.com>
2020-01-11 18:21:09 +0900

    Update Lesson10_01_MinPerimeterRectangle.py

--- v. Algorithm/C. Codility/a. Lessons/Lesson10_01_MinPerimeterRectangle.py
+++ v. Algorithm/C. Codility/a. Lessons/Lesson10_01_MinPerimeterRectangle.py
@@ -7,3 +7,14 @@ def solution(N):
         if b == 0:
             answer.append(2*(a+i))
     return min(answer)
+
+# 50%  https://app.codility.com/demo/results/training763JYY-NQ8/
+
+def solution(N):
+    answer = 4 * N
+    for i in range(1, int(N / 2) + 1):
+        a, b = divmod(N, i)
+        if b == 0:
+            if 2 * (i + a) < answer:
+                answer = 2 * (i + a)
+    return answer
