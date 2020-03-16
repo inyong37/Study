2e5c13064a01a7450e00ea9b6574d117bec9e84c
Inyong Hwang <inyong1020@gmail.com>
2020-03-16 10:09:14 +0900

    Renamed 'MY', 'Computer', and 'Framework' with adding '_' in front of them.

--- Framework/Keras/KERAS_Applications_Example_5.py
+++
@@ -1,9 +0,0 @@
-## 5. Build InceptionV3 over a custom input tensor
-
-from keras.applications.inception_v3 import InceptionV3
-from keras.layers import Input
-
-# this could also be the output a different Keras model or layer
-input_tensor = Input(shape=(224, 224, 3))  # this assumes K.image_data_format() == 'channels_last'
-
-model = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=True)
