da6a344819e43b44277440f6fa0d4cd0d3e268ad
Inyong Hwang <inyong1020@gmail.com>
2020-03-17 15:18:59 +0900

    Made python's module deque.py

---
+++ VI. Language/iii. Python/collections_deque.py
@@ -0,0 +1,13 @@
+import collections
+deq = collections.deque(['a', 'b', 'c', 'd', 'e'])
+deq.rotate(1)
+print('deq >>', ' '.join(deq))
+deq2 = collections.deque(['a', 'b', 'c', 'd', 'e'])
+deq2.rotate(2)
+print('deq2 >>', ' '.join(deq2))
+deq3 = collections.deque(['a', 'b', 'c', 'd', 'e'])
+deq3.rotate(-1)
+print('deq3 >>', ' '.join(deq3))
+deq4 = collections.deque(['a', 'b', 'c', 'd', 'e'])
+deq4.rotate(-2)
+print('deq4 >>', ' '.join(deq4))
