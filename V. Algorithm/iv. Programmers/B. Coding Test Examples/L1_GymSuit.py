d3651a3357cf6b910ca4de4ea73952e61cc82439
Inyong Hwang <inyong1020@gmail.com>
2020-03-30 20:39:45 +0900

    Create L1_GymSuit.py

---
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L1_GymSuit.py
@@ -0,0 +1,143 @@
+# 2020-03-30-Mon
+
+# Try 1 8/12
+
+def solution(n, lost, reserve):
+    students = dict()
+    # check clothes for each student
+    for i in range(n):
+        students[str(i)] = 1
+        if i in lost:
+            students[str(i)] -= 1
+        if i in reserve:
+            students[str(i)] += 1
+    # borrow
+    clothes = students
+    for i in range(n):
+        # only can check after
+        if i == 0:
+            # lost
+            if students[str(i)] == 0:
+                # can borrow
+                if students[str(i+1)] == 2:
+                    clothes[str(i)] += 1
+                    students[str(i+1)] -= 1
+                # can't borrow
+                else:
+                    clothes[str(i)] == 0
+            # didn't loss
+            else:
+                clothes[str(i)] == 1
+        # only can check before
+        elif i == n-1:
+            # lost
+            if students[str(i)] == 0:
+                # can borrow
+                if students[str(i-1)] == 2:
+                    clothes[str(i)] += 1
+                    students[str(i-1)] -= 1
+                # can't borrow
+                else:
+                    clothes[str(i)] == 0
+            # didn't loss
+            else:
+                pass
+                # clothes[str(i)] == 1
+        # can compare with before and after number
+        else:
+            # lost
+            if students[str(i)] == 0:
+                if students[str(i-1)] == 2:
+                    clothes[str(i)] += 1
+                    students[str(i-1)] -= 1
+                elif students[str(i+1)] == 2:
+                    clothes[str(i)] += 1
+                    students[str(i+1)] -= 1
+                else:
+                    pass
+                    # clothes[str(i)] == 0
+            # didn't loss
+            else:
+                pass
+                # clothes[str(i)] == 1
+    # Count students who have clothes
+    count = 0
+    for i in range(n):
+        if clothes[str(i)] >= 1:
+            count += 1
+        else:
+            pass
+    return count
+
+#  Try 2 8/12
+
+def solution(n, lost, reserve):
+    students = dict()
+    # check clothes for each student
+    for i in range(n):
+        students[str(i)] = 1
+        if i in lost:
+            students[str(i)] -= 1
+        if i in reserve:
+            students[str(i)] += 1
+    # borrow
+    clothes = students
+    for i in range(n):
+        # only can check after
+        if i == 0:
+            # lost
+            if students[str(i)] == 0:
+                # can borrow
+                if students[str(i+1)] == 2:
+                    students[str(i)] += 1 # clothes[str(i)] += 1
+                    students[str(i+1)] -= 1
+                # can't borrow
+                else:
+                    pass
+                    # clothes[str(i)] == 0
+            # didn't loss
+            else:
+                pass
+                # clothes[str(i)] == 1
+        # only can check before
+        elif i == n-1:
+            # lost
+            if students[str(i)] == 0:
+                # can borrow
+                if students[str(i-1)] == 2:
+                    students[str(i)] += 1# clothes[str(i)] += 1
+                    students[str(i-1)] -= 1
+                # can't borrow
+                else:
+                    pass
+                    # clothes[str(i)] == 0
+            # didn't loss
+            else:
+                pass
+                # clothes[str(i)] == 1
+        # can compare with before and after number
+        else:
+            # lost
+            if students[str(i)] == 0:
+                if students[str(i-1)] == 2:
+                    students[str(i)] += 1# clothes[str(i)] += 1
+                    students[str(i-1)] -= 1
+                elif students[str(i+1)] == 2:
+                    students[str(i)] += 1# clothes[str(i)] += 1
+                    students[str(i+1)] -= 1
+                else:
+                    pass
+                    # clothes[str(i)] == 0
+            # didn't loss
+            else:
+                pass
+                # clothes[str(i)] == 1
+    # Count students who have clothes
+    count = 0
+    for i in range(n):
+        if students[str(i)] >= 1:
+        # if clothes[str(i)] >= 1:
+            count += 1
+        else:
+            pass
+    return count
