d2d7bc96b6f742bacf0b86cab34a08bffe9dc8fa
Inyong Hwang <inyong1020@gmail.com>
2020-04-15 23:33:19 +0900

    Update L2_FineSquare.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_FineSquare.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_FineSquare.py
@@ -36,3 +36,16 @@ def solution(w,h):
         # passing (0, h), (w, 0) -> m=(0-h)/(w-0)=-1*(h/w)  -> (y)=-1*(h/w)(x-w) -> y = -1*h/w*x+h
         for i in range(0, w):
             print(i, -1*h/w*i+h)
+
+# Try 3
+
+from math import gcd
+
+def solution(w,h):
+    if w == h:
+        return w * (w - 1)
+    else:
+        d = gcd(w, h) # d = greates common dividor
+        return (w * h) - (w + h - d)
+
+# Reference: https://leedakyeong.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%A9%80%EC%A9%A1%ED%95%9C-%EC%82%AC%EA%B0%81%ED%98%95-in-python
