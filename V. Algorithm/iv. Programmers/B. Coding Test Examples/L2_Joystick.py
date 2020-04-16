e699e73e4c570ab3936ef9d46b3f1599ae91d14c
Inyong Hwang <inyong1020@gmail.com>
2020-04-16 13:39:43 +0900

    Update L2_Joystick.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_Joystick.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_Joystick.py
@@ -7,11 +7,11 @@ from string import ascii_uppercase
 def solution(nam):
     # input  : nam (name)
     # output : cnt (count to make name)
-    alp = list(ascii_uppercase) # alpabet list
-    now = alp[0]                # now alpabet
+    alp = list(ascii_uppercase) # alphabet list
+    now = alp[0]                # now alphabet
     idx = 0                     # now index
     cnt = 0
-    lng = 25                    # length of alpabet list
+    lng = 25                    # length of alphabet list
     haf = 13                    # half length
     for c in nam:
         nxt = alp.index(c)      # next index
@@ -25,4 +25,50 @@ def solution(nam):
         now = alp[idx]
         print(cnt, now)
         cnt += 1
+    return cnt
+
+# Try 2
+
+from string import ascii_uppercase
+
+def solution(nam):
+    # input  : nam (name)
+    # output : cnt (count to make name)
+    alp = list(ascii_uppercase) # alphabet list
+    nam = list(nam)
+    now = alp[0] * len(nam)     # now alphabet
+    cnt = 0
+    idx = 0
+    lng = 26                    # length of alphabet
+    haf = 13                    # half length
+    while True:
+        
+        if nam[idx] != alp[0]:
+            if idx > 13:
+                cnt += 26 - idx
+            else:
+                cnt += idx
+            nam[idx] = 'A'
+        
+        ridx = 1
+        lidx = 1
+        
+        if now == nam:
+            break
+        else:
+            for i in range(1, len(nam)):
+                if nam[idx+1] == 'A':
+                    ridx += 1
+                else:
+                    break
+                if nam[idx-1] == 'A':
+                    lidx += 1
+                else:
+                    break
+            if ridx > lidx:
+                cnt += lidx
+                idx -= ridx
+            else:
+                cnt += ridx
+                idx == ridx
     return cnt
