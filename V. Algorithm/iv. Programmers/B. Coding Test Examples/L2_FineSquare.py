0c782b72639a0238bdc1aba8213382837b202dd7
Inyong Hwang <inyong1020@gmail.com>
2020-04-16 10:00:04 +0900

    Update L2_FineSquare.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_FineSquare.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_FineSquare.py
@@ -49,3 +49,10 @@ def solution(w,h):
         return (w * h) - (w + h - d)
 
 # Reference: https://leedakyeong.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%A9%80%EC%A9%A1%ED%95%9C-%EC%82%AC%EA%B0%81%ED%98%95-in-python
+
+# Try 4
+
+from math import gcd
+
+def solution(w,h):
+    return (w * h) - (w + h - gcd(w, h))
