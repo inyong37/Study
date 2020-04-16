0f188fc43c9da22121bbb3916e93fa6d8f56bfde
Inyong Hwang <inyong1020@gmail.com>
2020-04-16 10:37:37 +0900

    Update L2_SkillTree.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_SkillTree.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_SkillTree.py
@@ -27,3 +27,26 @@ def solution(s, sts):
     return cnt
 
 # Try 2
+
+def solution(s, sts):
+    # input  : s (skill), sts (skill_trees)
+    # output : cnt (correct skill_trees)
+    cnt = 0
+    for i in sts:
+        flg = True # flag
+        cmp = ''
+        for item in i:
+            if item in s:
+                cmp += item
+        
+        for j in range(len(cmp)):
+            if cmp[j] != s[j]:
+                flg = False
+                break
+                
+        if flg:
+            cnt += 1
+        
+    return cnt
+
+# Reference: https://yorr.tistory.com/3
