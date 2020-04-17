570e2d4c29008a0f695d8090bf791d36d54663bf
Inyong Hwang <inyong1020@gmail.com>
2020-04-17 10:11:45 +0900

    Update L2_Joystick.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_Joystick.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_Joystick.py
@@ -72,3 +72,33 @@ def solution(nam):
                 cnt += ridx
                 idx == ridx
     return cnt
+
+# Try 3
+
+def solution(name):
+    answer = 0
+    name=list(name)
+    index=0
+    while(True):
+        right=1
+        left=1
+        if name[index] != 'A': 
+            updown = min(ord(name[index])-ord('A'),(ord('Z')-ord(name[index])+1))
+            answer += updown
+        name[index] = 'A'
+        if name == ["A"]*len(name): break
+        for i in range(1,len(name)):
+            if name[index+i]=="A": right+=1
+            else: break
+        for i in range(1,len(name)):
+            if name[index-i]=="A": left+=1
+            else: break
+        if right>left:
+            answer+=left
+            index-=left
+        else:
+            answer+=right
+            index+=right
+    return answer
+
+# Reference: http://blog.naver.com/PostView.nhn?blogId=jaeyoon_95&logNo=221740770765&categoryNo=74&parentCategoryNo=0&viewDate=&currentPage=1&postListTopCurrentPage=1&from=postView
