639285fbeb489ebfeabc1a81710c0671dab86fbc
Inyong Hwang <inyong1020@gmail.com>
2020-04-18 21:31:22 +0900

    Update L2_ParenthesisConvert.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_ParenthesisConvert.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_ParenthesisConvert.py
@@ -57,3 +57,60 @@ def solution(p):
             return tmp
 
 # Reference: https://inspirit941.tistory.com/m/104
+
+# balanced string
+def balance(string):
+    cnt = 0
+    tmp = list()
+    for idx, itm in enumerate(string):
+        if itm == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            return idx
+        else:
+            pass
+
+
+# righted string
+def right(string):
+    tmp = list()
+    for itm in string:
+        if itm == '(':
+            tmp.append(itm)
+        else:
+            if len(tmp) == 0:
+                return False
+            else:
+                pass
+            tmp.pop()
+    if len(tmp) != 0:
+        return False
+    else:
+        pass
+    return True
+    
+
+def solution(p):
+    if p == '':
+        return p
+    elif right(p):
+        return p
+    else:
+        u, v = p[:balance(p)+1], p[balance(p)+1:]
+        if right(u):
+            string = solution(v)
+            return u + string
+        else:
+            ans = '('
+            ans += solution(v)
+            ans += ')'
+            u = list(u[1:-1])
+            for idx in range(len(u)):
+                if u[idx] == '(':
+                    u[idx] = ')'
+                else:
+                    u[idx] = '('
+            ans += ''.join(u)
+            return ans
