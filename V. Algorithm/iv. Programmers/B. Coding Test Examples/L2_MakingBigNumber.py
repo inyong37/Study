6e85ca4389cc0a3210309ed851d25afc4d6675f4
Inyong Hwang <inyong1020@gmail.com>
2020-04-19 23:50:14 +0900

    Update L2_MakingBigNumber.py

--- V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_MakingBigNumber.py
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_MakingBigNumber.py
@@ -12,3 +12,20 @@ def solution(number, k):
 
 # Try 2
 
+def solution(number, k):
+    ans = list()
+    for idx, itm in enumerate(number):
+        while ans and ans[-1] < itm and k > 0:
+            ans.pop()
+            k -= 1
+        if k == 0:
+            ans += number[idx:]
+            break
+        ans.append(itm)
+    if k > 0:
+        ans = ans[:-k]
+    else:
+        pass
+    return ''.join(ans)
+
+# Reference: https://gurumee92.tistory.com/162
