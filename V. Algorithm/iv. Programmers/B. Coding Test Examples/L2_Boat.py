d4ece4a2b08309baff81c3c7cb579c87f9fa764b
Inyong Hwang <inyong1020@gmail.com>
2020-04-20 10:21:33 +0900

    Create L2_Boat.py

---
+++ V. Algorithm/iv. Programmers/B. Coding Test Examples/L2_Boat.py
@@ -0,0 +1,18 @@
+# 2020-04-20-Mon
+
+def solution(people, limit):
+    people.sort() # sort small > big
+    cnt = 0
+    ldx = 0 # light index
+    hdx = len(people) - 1 # heavy index
+    cnt = 0
+    while ldx < hdx:
+        if people[ldx] + people[hdx] <= limit:
+            cnt += 1
+            ldx += 1
+            hdx -= 1
+        else:
+            hdx -= 1
+    return len(people) - cnt
+
+# Reference: https://codedrive.tistory.com/46
