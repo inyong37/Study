954f379bf46906140d959adc2948b39b71b0f324
Inyong Hwang <inyong1020@gmail.com>
2020-06-10 17:59:51 +0900

    Create 2156.py

--- V. Algorithm/v. BOJ/Dynamic Programming/2156.py
+++ V. Algorithm/v. BOJ/Dynamic Programming/2156.py
@@ -1,3 +1,15 @@
 n = int(input())
-for i in range(0, n):
-    print()
+amount = [int(input()) for _ in range(n)]
+dp = list()
+dp.append(0)
+dp.append(amount[0])
+if n > 1:
+    dp.append(amount[0] + amount[1])
+
+for i in range(3, n + 1):
+    case_1 = amount[i-1] + dp[i-2]
+    case_2 = amount[i-1] + amount[i-2] + dp[i-3]
+    case_3 = dp[i-1]
+    dp.append(max(case_1, case_2, case_3))
+
+print(dp[n])
