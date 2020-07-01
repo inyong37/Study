35f5aec7506af6228a1c85a2ba25e71b874e9b9d
Inyong Hwang <inyong1020@gmail.com>
2020-07-01 13:00:18 +0900

    Updated Book-Keras Chapter_2.py

--- II. Book/iii. Korean/코딩셰프의 3분 딥러링, 케라스맛/Chapter 2.py
+++ II. Book/iii. Korean/코딩셰프의 3분 딥러링, 케라스맛/Chapter 2.py
@@ -13,3 +13,98 @@ def ANN_models_func(Nin, Nh, Nout):
     model = models.Model(x, y)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
+
+
+def ANN_seq_func(Nin, Nh, Nout):
+    model = models.Sequential()
+    model.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+    model.add(layers.Dense(Nout, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+class ANN_models_class(models.Model):
+    def __init__(self, Nin, Nh, Nout):
+        hidden = layers.Dense(Nh)
+        output = layers.Dense(Nout)
+        relu = layers.activations('relu')
+        softmax = layers.Activation('softmax')
+
+        x = layers.Input(shape=(Nin,))
+        h = relu(hidden(x))
+        y = softmax(Nout)
+
+        super().__init__(x, y)
+        self.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+class ANN_seq_class(models.Sequential):
+    def __init__(self, Nin, Nh, Nout):
+        super().__init__()
+        self.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+        self.add(layers.Dense(Nout, activation='softmax'))
+        self.compile(loss='categorical_crossentropy', optmizer='adam', metrics=['accuracy'])
+
+
+import numpy as np
+from keras import datasets
+from keras.utils import np_utils
+
+
+def Data_func():
+    (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+    Y_train = np_utils.to_categorical(y_train)
+    Y_test = np_utils.to_categorical(y_test)
+
+    L, W, H = X_train.shape
+    X_train = X_train.reshape(-1, W * H)
+    X_test = X_test.reshape(-1, W * H)
+
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+
+    return (X_train, Y_train), (X_test, Y_test)
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc=0)
+
+
+def plot_acc(history):
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc=0)
+
+
+def main():
+    Nin = 785
+    Nh = 100
+    number_of_class = 10
+    Nout = number_of_class
+
+    model = ANN_seq_class(Nin, Nh, Nout)
+    (X_train, Y_train), (X_test, Y_test) = Data_func()
+
+    history = model.fit(X_train, Y_train, epoch=15, batch_size=100, validation_split=0.2)
+    performance_test = model.evaluate(X_test, Y_test, batch_size=100)
+    print('Test Loss and Accuracy ->', performance_test)
+
+    plot_loss(history)
+    plt.show()
+    plot_acc(history)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
