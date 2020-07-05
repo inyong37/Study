d72ea7faef9c6be5224d78394ab5c3b0c030d2be
inyong37 <inyong1020@gmail.com>
2020-07-05 13:56:28 +0900

    Updated Book-Keras Chapter_6_1_AE_FC_MNIST.py

--- II. Book/iii. Korean/코딩셰프의 3분 딥러링, 케라스맛/Chapter_6_1_AE_FC_MNIST.py
+++ II. Book/iii. Korean/코딩셰프의 3분 딥러링, 케라스맛/Chapter_6_1_AE_FC_MNIST.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 # Modified Author : Inyong Hwang (inyong1020@gmail.com)
-# Date            : 2020-07-03-Fri
+# Date            : 2020-07-05-Sun
 # Title           : 코딩셰프의 분 딥러닝, 케라스맛 Chapter 6. 케라스로 구현하는 AE(오토인코더)
 
 
-import seaborn as sns
-import pandas as pd
 import numpy as np
-from sklearn import model_selection
 from keras import models, layers
+from keras.datasets import mnist
 import matplotlib.pyplot as plt
+import inspect
+import os
 
 
 def plot_loss(history):
@@ -41,18 +41,19 @@ class AE(models.Model):
 
         self.x = x
         self.z = z
+        self.z_dim = z_dim
 
+        self.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
 
-def Encoder(self):
-    return models.Model(self.x, self.z)
+    def Encoder(self):
+        return models.Model(self.x, self.z)
 
-
-def Decoder(self):
-    z_shape = (self.z_dim,)
-    z = layers.Input(shape=z_shape)
-    y_layer = self.layers[-1]
-    y = y_layer(z)
-    return models.Model(z, y)
+    def Decoder(self):
+        z_shape = (self.z_dim,)
+        z = layers.Input(shape=z_shape)
+        y_layer = self.layers[-1]
+        y = y_layer(z)
+        return models.Model(z, y)
 
 
 (X_train, _), (X_test, _) = mnist.load_data()
@@ -61,114 +62,57 @@ X_train = X_train.astype('float32') / 255.
 X_test = X_test.astype('float32') / 255.
 X_train = X_train.reshape((len(X_train), np.prod(X_train.shape[1:])))
 X_test = X_test.reshape((len(X_test), np.prod(X_test.shape[1:])))
-        
+print(X_train.shape)
+print(X_test.shape)
+
+
+def show_ae(autoencoder):
+    encoder = autoencoder.Encoder()
+    decoder = autoencoder.Decoder()
+
+    encoded_imgs = encoder.predict(X_test)
+    decoded_imgs = decoder.predict(encoded_imgs)
+
+    n = 10
+    plt.figure(figsize=(20, 6))
+    for i in range(n):
+
+        ax = plt.subplot(3, n, i + 1)
+        plt.imshow(X_test[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        ax = plt.subplot(3, n, i + 1 + n)
+        plt.stem(encoded_imgs[i].reshape(-1))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        ax = plt.subplot(3, n, i + 1 + n + n)
+        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
 
 def main():
-    machine = Machine()
-    machine.run(epochs=400)
+    x_nodes = 784
+    z_dim = 36
 
+    autoencoder = AE(x_nodes, z_dim)
 
-class Machine:
-    def __init__(self):
-        self.data = Dataset()
-        shape = self.data.X.shape[1:]
-        self.model = rnn_model(shape)
+    history = autoencoder.fit(X_train, X_train, epochs=10, batch_size=256, shuffle=True, validation_data=(X_test, X_test))
 
-    def run(self, epochs=400):
-        d = self.data
-        X_train, X_test = d.X_train, d.X_test
-        y_train, y_test = d.y_train, d.y_test
-        X, y = d.X, d.y_test
-        m = self.model
-        h = m.fit(X_train, y_train, epochs=epochs, validation_data=[X_test, y_test], verbose=0)
-
-        plot_loss(h)
-        plt.title('History of training')
-        plt.show()
-
-        yp = m.predict(X_test)
-        print('Loss:', m.evaluate(X_test, y_test))
-        plt.plot(yp, label='Original')
-        plt.plot(y_test, label='Prediction')
-        plt.legend(loc=0)
-        plt.title('Validation Results')
-        plt.show()
-
-        yp = m.predict(X_test).reshape(-1)
-        print('Loss:', m.evaluate(X_test, y_test))
-        print(yp.shape, y_test.shape)
-
-        df = pd.DataFrame()
-        df['Sample'] = list(range(len(y_test))) * 2
-        df['Normalized #Passengers'] = np.concatenate([y_test, yp], axis=0)
-        df['Type'] = ['Original'] * len(y_test) + ['Prediction'] * len(yp)
-
-        plt.figure(figsize=(7, 5))
-        sns.barplot(x='Sample', y='Normalized #Passengers', hue='Type', data=df)
-        plt.ylabel('Normalized #Passengers')
-        plt.show()
-
-        yp = m.predict(X)
-
-        plt.plot(yp, label='Original')
-        plt.plot(y, label='Prediction')
-        plt.legend(loc=0)
-        plt.title('All Results')
-        plt.show()
-
-
-def rnn_model(shape):
-    m_x = layers.Input(shape=shape)
-    m_h = layers.LSTM(10)(m_x)
-    m_y = layers.Dense(1)(m_h)
-    m = models.Model(m_x, m_y)
-
-    m.compile('adam', 'mean_squared_error')
-
-    m.summary()
-    return m
-
-
-class Dataset:
-    def __init__(self, fname='international-airline-passengers.csv', D=12):
-        data_dn = load_data(fname=fname)
-        X, y = get_Xy(data_dn, D=D)
-        X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=43)
-
-        self.X, self.y = X, y
-        self.X_train, self.X_test, self.y_train, self.y_test = X_train, X_test, y_train, y_test
-
-
-def load_data(fname='international-airline-passengers.csv'):
-    dataset = pd.read_csv(fname, usecols=[1], engine='python', skipfooter=3)
-    data = dataset.values.reshape(-1)
-    plt.plot(data)
-    plt.xlabel('Time'); plt.ylabel('#Passengers')
-    plt.title('Original Data')
+    plot_acc(history)
+    plt.show()
+    plot_loss(history)
     plt.show()
 
-    data_dn = (data - np.mean(data)) / np.std(data) / 5
-    plt.plot(data_dn)
-    plt.xlabel('Time'); plt.ylabel('Normalized #Passengers')
-    plt.title('Noramilzed data by $E[]$ and $5\sigma$')
+    show_ae(autoencoder)
     plt.show()
-
-    return data_dn
-
-
-def get_Xy(data, D=12):
-    X_l = []
-    y_l = []
-    N = len(data)
-    assert N > D, 'N should be larger than D, where N is len(data'
-    for ii in range(N-D-1):
-        X_l.append(data[ii:ii+D])
-        y_l.append(data[ii+D])
-    X = np.array(X_l)
-    X = X.reshape(X.shape[0], X.shape[1], 1)
-    y = np.array(y_l)
-    print(X.shape, y.shape)
-    return X, y
 
 
 if __name__ == '__main__':
