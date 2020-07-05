8326c07633bb28f4ac85182445b28f6c5082f4bd
inyong37 <inyong1020@gmail.com>
2020-07-05 16:20:47 +0900

    Updated Book-Keras Chatper_7_1_GAN_DNN.py

--- II. Book/iii. Korean/코딩셰프의 3분 딥러링, 케라스맛/Chapter_7_1_GAN_DNN.py
+++ II. Book/iii. Korean/코딩셰프의 3분 딥러링, 케라스맛/Chapter_7_1_GAN_DNN.py
@@ -41,112 +41,170 @@ def main():
     machine.run(n_repeat=200, n_show=200, n_test=100)
 
 
-class DATA:
-    def __init__(self):
-        num_classes = 10
-        (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
-        img_rows, img_cols = x_train.shape[1:]
-        if backend.image_data_format() == 'channels_first':
-            x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-            x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-            input_shape = (1, img_rows, img_cols)
-        else:
-            x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-            x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-            input_shape = (img_rows, img_cols, 1)
-
-        x_train = x_train.astype('float32')
-        x_test = x_test.astype('float32')
-        x_train /= 255
-        x_test /= 255
-
-        y_train = keras.utils.to_categorical(y_train, num_classes)
-        y_test = keras.utils.to_categorical(y_test, num_classes)
-
-        self.input_shape = input_shape
-        self.num_clsses = num_classes
-        self.x_train, self.y_train = x_train, y_train
-        self.x_test, self.y_test = x_test, y_test
-
-def Conv2D(filters, kernel_size, padding='same', activation='relu'):
-    return layers.Conv2D(filters, kernel_size, padding=padding, activation=activation)
+class Data:
+    def __init__(self, mu, sigma, ni_D):
+        self.real_sample = lambda n_batch: np.random.normal(mu, sigma, (n_batch, ni_D))
+        self.in_sample = lambda n_batch: np.random.rand(n_batch, ni_D)
 
 
-class AE(models.Model):
-    def __init__(self, org_shape=(1, 28, 28)):
-        original = layers.Input(shape=org_shape)
+class Machine:
+    def __init__(self, n_batch=10, ni_D=100):
+        data_mean = 4
+        data_stddev = 1.25
 
-        x = Conv2D(4, (3, 3))(original)
-        x = layers.MaxPooling2D((2, 2), padding='same')(x)
+        self.n_iter_D = 1
+        self.n_iter_G = 5
 
-        x = Conv2D(8, (3, 3))(x)
-        x = layers.MaxPooling2D((2, 2), padding='same')(x)
+        self.data = Data(data_mean, data_stddev, ni_D)
+        self.gan = GAN(ni_D=ni_D, nh_D=50, nh_G=50)
 
-        z = Conv2D(1, (7, 7))(x)
+        self.n_batch = n_batch
+        # self.ni_D = ni_D
 
-        y = Conv2D(16, (3, 3))(z)
-        y = layers.UpSampling2D((2, 2))(y)
+    def train_D(self):
+        gan = self.gan
+        n_batch = self.n_batch
+        data = self.data
 
-        y = Conv2D(8, (3, 3))(y)
-        y = layers.UpSampling2D((2, 2))(y)
+        Real = data.real_sample(n_batch)
+        Z = data.in_sample(n_batch)
+        Gen = gan.G.predict(Z)
+        gan.D.trainalbe = True
+        gan.D_train_on_batch(Real, Gen)
 
-        y = Conv2D(4, (3, 3))(y)
+    def train_GD(self):
+        gan = self.gan
+        n_batch = self.n_batch
+        data = self.data
+        Z = data.in_sample(n_batch)
 
-        decoded = Conv2D(1, (3, 3), activation='sigmoid')(y)
+        gan.D_trainable = False
+        gan.GD_train_on_batch(Z)
 
-        super().__init__(original, decoded)
-        self.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
+    def train_each(self):
+        for it in range(self.n_iter_D):
+            self.train_D()
+        for it in range(self.n_iter_G):
+            self.train_GD()
+
+    def train(self, epochs):
+        for epoch in range(epochs):
+            self.train_each()
+
+    def test(self, n_test):
+        gan = self.gan
+        data = self.data
+        Z = data.in_sample(n_test)
+        Gen = gan.G.predict(Z)
+        return Gen, Z
+
+    def show_hist(self, Real, Gen, Z):
+        plt.hist(Real.reshape(-1), histtype='step', label='Real')
+        plt.hist(Gen.reshape(-1), histtype='step', label='Generated')
+        plt.hist(Z.reshape(-1), histtype='step', label='Input')
+        plt.legend(loc=0)
+
+    def test_and_show(self, n_test):
+        data = self.data
+        Gen, Z = self.test(n_test)
+        Real = data.real_sample(n_test)
+        self.show_hist(Real, Gen, Z)
+        Machine.print_stat(Real, Gen)
+
+    def run_epochs(self, epochs, n_test):
+        self.train(epochs)
+        self.test_and_show(n_test)
+
+    def run(self, n_repeat=200, n_show=200, n_test=100):
+        for ii in range(n_repeat):
+            print('Stage', ii, '(Epoch: {})'.format(ii * n_show))
+            self.run_epochs(n_show, n_test)
+            plt.show()
+
+    @staticmethod
+    def print_stat(Real, Gen):
+        def stat(d):
+            return  (np.mean(d), np.std(d))
+        print('Mean and Std of Real:', stat(Real))
+        print('Mean and Std of Gen:', stat(Gen))
 
 
+def add_decorate(x):
+    m = K.mean(x, axis=-1, keepdims=True)
+    d = K.square(x - m)
+    return K.concatenate([x, d], axis=-1)
 
 
-
-def show_ae(autoencoder, data):
-    x_test = data.x_test
-    decoded_imgs = autoencoder.predict(x_test)
-    print(decoded_imgs.shape, data.x_test.shape)
-
-    if backend.image_data_format() == 'channels_first':
-        N, c_ch, n_i, n_j = x_test.shape
-    else:
-        N, n_i, n_j, n_ch = x_test.shape
-
-    x_test = x_test.reshape(N, n_i, n_j)
-    decoded_imgs = decoded_imgs.reshape(decoded_imgs.shape[0], n_i, n_j)
-
-    n = 10
-    plt.figure(figsize=(20, 4))
-    for i in range(n):
-
-        ax = plt.subplot(2, n, i + 1)
-        plt.imshow(x_test[i])
-        # plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        ax = plt.subplot(2, n, i + 1 + n)
-        plt.imshow(decoded_imgs[i])
-        # plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-    plt.savefig(filename.replace('.py', '') + '_ae.png', dpi=300)
-    plt.show()
+def add_decorate_shape(input_shape):
+    shape = list(input_shape)
+    assert len(shape) == 2
+    shape[1] *= 2
+    return tuple(shape)
 
 
-def main(epochs=20, batch_size=128):
-    data = DATA()
-    autoencoder = AE(data.input_shape)
+lr = 2e-4
+adam = Adam(lr=lr, beta_1=0.9, beta_2=0.999)
 
-    history = autoencoder.fit(data.x_train, data.x_train, epochs=epochs, batch_size=batch_size, shuffle=True, validation_split=0.2)
 
-    plot_acc(history)
-    plt.show()
-    plot_loss(history)
-    plt.show()
+def model_complie(model):
+    return model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-    show_ae(autoencoder, data)
-    plt.show()
+
+class GAN:
+    def __init__(self, ni_D, nh_D, nh_G):
+        self.ni_D = ni_D
+        self.nh_D = nh_D
+        self.nh_G = nh_G
+
+        self.D = self.gen_D()
+        self.G = self.gen_G()
+        self.GD = self.make_GD()
+
+    def gen_D(self):
+        ni_D = self.ni_D
+        nh_D = self.nh_D
+        D = models.Sequential()
+        D.add(Lambda(add_decorate, output_shape=add_decorate_shape, input_shape=(ni_D,)))
+        D.add(Dense(nh_D, activation='relu'))
+        D.add(Dense(1, activation='sigmoid'))
+
+        model_complie(D)
+        return D
+
+    def gen_G(self):
+        ni_D = self.ni_D
+        nh_G = self.nh_D
+
+        G = models.Sequential()
+        G.add(Reshape((ni_D, 1), input_shape=(ni_D,)))
+        G.add(Conv1D(nh_G, 1, activation='relu'))
+        G.add(Conv1D(nh_G, 1, activation='sigmoid'))
+        G.add(Conv1D(1, 1))
+        G.add(Flatten())
+
+        model_complie(G)
+        return G
+
+    def make_GD(self):
+        G, D = self.G, self.D
+        GD = models.Sequential()
+        GD.add(G)
+        GD.add(D)
+        D.trainable = False
+        model_complie(GD)
+        D.trainable = True
+        return GD
+
+    def D_train_on_batch(self, Real, Gen):
+        D = self.D
+        X = np.concatenate([Real, Gen], axis=0)
+        y = np.array([1] * Real.shape[0] + [0] * Gen.shape[0])
+        D.train_on_batch(X, y)
+
+    def GD_train_on_batch(self, Z):
+        GD = self.GD
+        y = np.array([1] * Z.shape[0])
+        GD.train_on_batch(Z, y)
 
 
 if __name__ == '__main__':
