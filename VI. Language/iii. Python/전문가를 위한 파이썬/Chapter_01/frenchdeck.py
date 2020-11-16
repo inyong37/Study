24b627ac26d56bcd8a05842f0f16e5955d571525
Inyong Hwang <inyong1020@gmail.com>
2020-11-16 13:19:10 +0900

    Study fluent python chapter 1

--- VI. Language/iii. Python/전문가를 위한 파이썬/Chapter_01/frenchdeck.py
+++ VI. Language/iii. Python/전문가를 위한 파이썬/Chapter_01/frenchdeck.py
@@ -6,13 +6,11 @@ Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11) + list('JQKA')]
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit)
-                      for suit in self.suits
-                      for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
