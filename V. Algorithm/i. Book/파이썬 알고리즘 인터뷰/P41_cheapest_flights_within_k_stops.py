5ff16df9d3ce83171ac80ee295db48c3aa4f72be
Inyong Hwang <inyong1020@gmail.com>
2021-03-03 00:24:22 +0900

    Create P41_cheapest_flights_within_k_stops.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/P41_cheapest_flights_within_k_stops.py
@@ -0,0 +1,21 @@
+import collections
+
+def findCheapestPrices(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+  graph = collections.deafaultdict(list)
+  # 그래프 인접 리스트 구성
+  for u, v, w in flights:
+    graph[u].append((v, w))
+  
+  # 큐 변수: [(가격, 정점, 남은 가능 경유지 수)]
+  Q = [(0, src, K)]
+  
+  # 우선순위 큐 최솟값 기준으로 도착점까지 최소 비용 판별
+  while Q:
+    price, node, K = heapq.heappop(Q)
+    if node == dst:
+      return price
+    if K >= 0:
+      for v, w in graph[node]:
+        alt = price + w
+        heapq.heappush(Q, (alt, v, k - 1))
+  return -1
