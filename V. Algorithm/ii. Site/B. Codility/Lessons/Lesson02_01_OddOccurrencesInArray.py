64e984cf9144a23d93127c5e87bc49d0dd7d172d
Inyong Hwang <inyong1020@gmail.com>
2021-03-17 13:59:34 +0900

    Update Lesson02_01_OddOccurrencesInArray.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson02_01_OddOccurrencesInArray.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson02_01_OddOccurrencesInArray.py
@@ -8,3 +8,10 @@ def solution(A):
             if A[i] != A[i+1]:
                 return A[i]
     return A[-1]
+
+# 2021-03-17-Tue_44% https://app.codility.com/demo/results/training6GPZPC-MA2/
+import collections
+
+def solution(A):
+    result = collections.Counter(A)
+    return result.popitem()[0]
