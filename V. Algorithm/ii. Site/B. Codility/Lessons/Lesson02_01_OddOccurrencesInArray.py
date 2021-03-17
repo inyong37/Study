37df154e1f70ac7fdbf5cbfe633422069cc418a5
Inyong Hwang <inyong1020@gmail.com>
2021-03-17 17:15:31 +0900

    Update Lesson02_01_OddOccurrencesInArray.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson02_01_OddOccurrencesInArray.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson02_01_OddOccurrencesInArray.py
@@ -9,7 +9,7 @@ def solution(A):
                 return A[i]
     return A[-1]
 
-# 2021-03-17-Tue_44% https://app.codility.com/demo/results/training6GPZPC-MA2/
+# 2021-03-17-Wed_44% https://app.codility.com/demo/results/training6GPZPC-MA2/
 import collections
 
 def solution(A):
@@ -17,7 +17,7 @@ def solution(A):
     return result.popitem()[0]
 
 
-# 2021-03-17-Tue_66% https://app.codility.com/demo/results/trainingD4W5BP-XJ9/ runtime error result[1] -> there might be 1/3/5... odd, 2 ,3, 4 
+# 2021-03-17-Wed_66% https://app.codility.com/demo/results/trainingD4W5BP-XJ9/ runtime error result[1] -> there might be 1/3/5... odd, 2 ,3, 4 
 import collections
 
 def solution(A):
@@ -25,7 +25,7 @@ def solution(A):
     result = {v: k for k, v in result.items()}
     return result[1]
 
-# 2021-03-17-Tue_100% https://app.codility.com/demo/results/trainingE8FHXR-E68/ O(N) or O(N*logN)
+# 2021-03-17-Wed_100% https://app.codility.com/demo/results/trainingE8FHXR-E68/ O(N) or O(N*logN)
 import collections
 
 def solution(A):
@@ -35,7 +35,7 @@ def solution(A):
         if key % 2 == 1:
             return val
 
-# 2021-03-17-Tue_100% https://app.codility.com/demo/results/trainingC7X2YY-DTT/
+# 2021-03-17-Wed_100% https://app.codility.com/demo/results/trainingC7X2YY-DTT/
 import collections
 
 def solution(A):
