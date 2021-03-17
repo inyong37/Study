93e00cfc03b4cbf1acd78912b636d3682c8fb9e0
Inyong Hwang <inyong1020@gmail.com>
2021-03-17 16:56:32 +0900

    Update Lesson03_01_ FrogJmp.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson03_01_ FrogJmp.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson03_01_ FrogJmp.py
@@ -4,3 +4,10 @@ import math
 
 def solution(X, Y, D):
     return math.ceil((Y - X) / D)
+
+# 2021-03-17-Tue_100% https://app.codility.com/demo/results/trainingMWVF2M-6HP/
+from math import ceil
+
+def solution(X, Y, D):
+    return ceil((Y - X) / D)
+    
