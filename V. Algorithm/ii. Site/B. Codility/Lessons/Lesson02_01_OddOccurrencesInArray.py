3ef5e40ce9d63708f99d6fc5a7770500fa01cd8c
Inyong Hwang <inyong1020@gmail.com>
2021-03-17 14:10:32 +0900

    Update Lesson02_01_OddOccurrencesInArray.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson02_01_OddOccurrencesInArray.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson02_01_OddOccurrencesInArray.py
@@ -15,3 +15,22 @@ import collections
 def solution(A):
     result = collections.Counter(A)
     return result.popitem()[0]
+
+
+# 2021-03-17-Tue_66% https://app.codility.com/demo/results/trainingD4W5BP-XJ9/ runtime error result[1] -> there might be 1/3/5... odd, 2 ,3, 4 
+import collections
+
+def solution(A):
+    result = collections.Counter(A)
+    result = {v: k for k, v in result.items()}
+    return result[1]
+
+# 2021-03-17-Tue_100% https://app.codility.com/demo/results/trainingE8FHXR-E68/ O(N) or O(N*logN)
+import collections
+
+def solution(A):
+    result = collections.Counter(A)
+    result = {v: k for k, v in result.items()}
+    for key, val in result.items():
+        if key % 2 == 1:
+            return val
