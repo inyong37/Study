ac45cd4a631977a63e84d6dae2034d606e09d0c9
Inyong Hwang <inyong1020@gmail.com>
2021-03-21 14:48:03 +0900

    Update Lesson05_03_MinAvgTwoSlice.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson05_03_MinAvgTwoSlice.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson05_03_MinAvgTwoSlice.py
@@ -30,3 +30,15 @@ def solution(A):
             value = (A[i] + A[i+1] + A[i+2])/3 < value
     
     return result
+
+# 2021-03-21-Sun_50% https://app.codility.com/demo/results/training4N92KD-ESA/ correctness 100%, performance 0%, timeout error
+def solution(A):
+    result = float('inf')
+    idx = 0
+    for p in range(len(A)):
+        for q in range(p+1, len(A)):
+            new = sum(A[p:q+1])/(q-p+1)
+            if result > new:
+                idx = p
+                result = new
+    return idx
