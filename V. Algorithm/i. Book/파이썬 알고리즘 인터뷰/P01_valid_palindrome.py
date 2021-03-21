e8b1752f06941fe64408b3b42d2058d5374341af
Inyong Hwang <inyong1020@gmail.com>
2021-03-21 21:23:21 +0900

    Update P01_valid_palindrome.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/P01_valid_palindrome.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/P01_valid_palindrome.py
@@ -1,1 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+        return True
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+        return True
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+        return s == s[::-1]
