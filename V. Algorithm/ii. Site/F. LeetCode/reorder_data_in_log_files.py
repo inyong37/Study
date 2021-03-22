fa7472e91039ab1919d85d6d472dfe16ab89db49
Inyong Hwang <inyong1020@gmail.com>
2021-03-22 22:01:00 +0900

    Create reorder_data_in_log_files.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/reorder_data_in_log_files.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
