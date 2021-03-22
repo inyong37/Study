a16762fae4636aa885adcc244782af7b66e7233b
Inyong Hwang <inyong1020@gmail.com>
2021-03-22 22:56:55 +0900

    Create 049_group_anagrams.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/049_group_anagrams.py
@@ -0,0 +1,6 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return anagrams.values()
