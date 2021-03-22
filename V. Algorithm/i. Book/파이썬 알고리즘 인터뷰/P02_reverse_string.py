248f278ad12a94d72aafe0ee30b1258d0d1bf77c
Inyong Hwang <inyong1020@gmail.com>
2021-03-22 22:03:28 +0900

    Update P02_reverse_string.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/P02_reverse_string.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/P02_reverse_string.py
@@ -1,12 +1,15 @@
-def reverseString(self, s: List[str]) -> None:
-  left, right = 0, len(s) - 1
-  while left < right:
-    s[left], s[right] = s[right], s[left]
-    left += 1
-    right -= 1
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
 
-def reverseString(self, s: List[str]) -> None:
-  s[:] = s[::-1]
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s[:] = s[::-1]
 
-def reverseString(self, s: List[str]) -> None:
-  s.reverse()
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
