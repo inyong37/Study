d01a216495666305e43018a70da5c5568571cd06
Inyong Hwang <inyong1020@gmail.com>
2021-03-24 02:15:01 +0900

    Create 001_two_sum.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/001_two_sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target - num], i]
+            nums_map[num] = i
