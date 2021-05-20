14f83ae905db21014a3a9175c70bd0e7d2fd532c
Inyong Hwang <inyong1020@gmail.com>
2021-05-20 23:22:05 +0900

    Update 042_trapping_rain_water.py

--- V. Algorithm/ii. Site/F. LeetCode/042_trapping_rain_water.py
+++ V. Algorithm/ii. Site/F. LeetCode/042_trapping_rain_water.py
@@ -17,3 +17,20 @@ class Solution:
                 volume += right_max - height[right]
                 right -= 1
         return volume
+
+    
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                if not len(stack):
+                    break
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+            stack.append(i)
+        return volume
