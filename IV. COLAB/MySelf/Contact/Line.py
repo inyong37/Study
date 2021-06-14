aece1b327380c8b80906a2601619f8febc498593
inyong37 <inyong1020@gmail.com>
2021-06-14 11:02:31 +0900

    Update todo list of contact in bot

---
+++ IV. COLAB/MySelf/Contact/Line.py
@@ -0,0 +1,7 @@
+# Line & KakaoTalk
+
+# TODO: get executable file path by finding(grep)
+# TODO: find line/kakao-talk api vs. handmade: login
+# TODO: screenshot
+# TODO: sending message to army
+# TODO: find line/kakao-talk api vs. handmade: sending check message to admin
