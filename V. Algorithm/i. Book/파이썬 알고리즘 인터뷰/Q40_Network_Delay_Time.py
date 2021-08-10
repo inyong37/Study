6668f9daad1f053a8d9168b96ee90dc5752004eb
Inyong Hwang <inyong1020@gmail.com>
2021-08-10 16:59:44 +0900

    Update and rename Q40_network_delay_time.py to Q40_Network_Delay_Time.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q40_Network_Delay_Time.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q40_Network_Delay_Time.py
@@ -1,3 +1,22 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+        Q = [(0, k)]
+        dist = collections.defaultdict(int)
+        while Q:
+            time, node = heapq.heappop(Q)
+            if node not in dist:
+                dist[node] = time
+                for v, w in graph[node]:
+                    alt = time + w
+                    heapq.heappush(Q, (alt, v))
+        if len(dist) == n:
+            return max(dist.values())
+        return -1
+
+'''
 import collections
 
 def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
@@ -23,3 +42,4 @@ def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
   if len(dist) == N:
     return max(dist.values())
   return -1
+'''
