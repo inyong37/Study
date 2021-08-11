1b4b1da4e0953b5a9803f44c4589138326394d66
Inyong Hwang <inyong1020@gmail.com>
2021-08-11 11:13:13 +0900

    Create 787_Cheapest_Flight_Within_K_Stops.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/787_Cheapest_Flight_Within_K_Stops.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
+        Q = [(0, src, k)]
+        while Q:
+            price, node, k = heapq.heappop(Q)
+            if node == dst:
+                return price
+            if k >= 0:
+                for v, w in graph[node]:
+                    alt = price + w
+                    heapq.heappush(Q, (alt, v, k - 1))
+        return -1
