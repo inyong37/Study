a2561c0da7c15c3395e72dd3921f0a9f4ae17a0a
Inyong Hwang <inyong1020@gmail.com>
2021-08-11 15:05:29 +0900

    Create Q43_Diameter_Of_Binary_Tree.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q43_Diameter_Of_Binary_Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    longest: int = 0
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return -1
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.longest = max(self.longest, left + right + 2)
+            return max(left, right) + 1
+        dfs(root)
+        return self.longest
