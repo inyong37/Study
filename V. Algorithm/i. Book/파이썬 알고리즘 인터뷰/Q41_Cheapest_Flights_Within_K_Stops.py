30f50edfa84cf6e028272476bde2019f12a2b7d0
Inyong Hwang <inyong1020@gmail.com>
2021-08-11 11:13:51 +0900

    Update and rename Q41_cheapest_flights_within_k_stops.py to Q41_Cheapest_Flights_Within_K_Stops.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q41_Cheapest_Flights_Within_K_Stops.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q41_Cheapest_Flights_Within_K_Stops.py
@@ -1,3 +1,20 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
+        Q = [(0, src, k)]
+        while Q:
+            price, node, k = heapq.heappop(Q)
+            if node == dst:
+                return price
+            if k >= 0:
+                for v, w in graph[node]:
+                    alt = price + w
+                    heapq.heappush(Q, (alt, v, k - 1))
+        return -1
+
+'''
 import collections
 
 def findCheapestPrices(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
@@ -19,3 +36,4 @@ def findCheapestPrices(self, n: int, flights: List[List[int]], src: int, dst: in
         alt = price + w
         heapq.heappush(Q, (alt, v, k - 1))
   return -1
+'''
