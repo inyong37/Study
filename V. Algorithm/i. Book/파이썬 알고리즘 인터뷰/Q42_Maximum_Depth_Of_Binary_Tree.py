260307b73ad52007310fd290d5067de943f61f99
Inyong Hwang <inyong1020@gmail.com>
2021-08-11 14:08:11 +0900

    Create Q42_Maximum_Depth_Of_Binary_Tree.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q42_Maximum_Depth_Of_Binary_Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+        return depth
+        
