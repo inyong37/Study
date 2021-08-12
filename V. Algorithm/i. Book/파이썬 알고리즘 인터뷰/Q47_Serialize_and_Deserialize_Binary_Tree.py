3f8ee8fd40802471bbb8398c663b2c630ca1ecda
Inyong Hwang <inyong1020@gmail.com>
2021-08-12 10:24:10 +0900

    Update Q47_Serialize_and_Deserialize_Binary_Tree.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q47_Serialize_and_Deserialize_Binary_Tree.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q47_Serialize_and_Deserialize_Binary_Tree.py
@@ -23,7 +23,8 @@ class Codec:
                 result.append(str(node.val))
             else:
                 result.append('#')
-            return ' '.join(result)
+        return ' '.join(result)
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -40,7 +41,7 @@ class Codec:
         while queue:
             node = queue.popleft()
             if nodes[index] is not '#':
-                node.left = TreeNode(int(nodex[index]))
+                node.left = TreeNode(int(nodes[index]))
                 queue.append(node.left)
             index += 1
             if nodes[index] is not '#':
@@ -48,7 +49,6 @@ class Codec:
                 queue.append(node.right)
             index += 1
         return root
-        
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
