0632eefe50f078c402ee751ceca60a22367e7e97
Inyong Hwang <inyong1020@gmail.com>
2021-08-12 12:01:14 +0900

    Create 110_Balanced_Binary_Tree.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/110_Balanced_Binary_Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check(root):
+            if not root:
+                return 0
+            left = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+        return check(root) != -1
+        
