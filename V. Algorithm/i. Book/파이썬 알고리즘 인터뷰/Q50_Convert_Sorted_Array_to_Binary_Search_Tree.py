727117f0befd1bfba8c9581f5aea415683b86f62
Inyong Hwang <inyong1020@gmail.com>
2021-08-12 15:36:47 +0900

    Create Q50_Convert_Sorted_Array_to_Binary_Search_Tree.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q50_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        mid = len(nums) // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1:])
+        return node
