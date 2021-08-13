1e0b1fa8905b5f16fc2e7a9e643550f59466febb
Inyong Hwang <inyong1020@gmail.com>
2021-08-13 15:45:57 +0900

    Create 00_Hello_World.ts

---
+++ V. Algorithm/ii. Site/A. Hacker Rank/30 Days of Code/TypeScript/00_Hello_World.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+function main() {
+    // Enter your code here
+    console.log("Hello, World.");
+    inputString = readLine();
+    console.log(inputString);
+}
