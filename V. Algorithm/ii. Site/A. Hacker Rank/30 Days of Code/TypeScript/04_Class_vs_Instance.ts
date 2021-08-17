7e5a495569525561f765069aabf2901b6b1f8ee5
Inyong Hwang <inyong1020@gmail.com>
2021-08-17 15:04:03 +0900

    Create 04_Class_vs_Instance.ts

---
+++ V. Algorithm/ii. Site/A. Hacker Rank/30 Days of Code/TypeScript/04_Class_vs_Instance.ts
@@ -0,0 +1,61 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+function Person(initAge: number){
+    let age: number;
+    if (initAge >= 0) {
+        age = initAge;
+    }
+    else {
+        age = 0;
+        console.log('Age is not valid, setting age to 0.');
+    }
+    
+    let amIOld = function(): void {
+        if (age < 13) {
+            console.log('You are young.');
+        }
+        else if (age >= 13 && age < 18) {
+            console.log('You are a teenager.');
+        }
+        else {
+            console.log('You are old.')
+        }
+    };
+    
+    let yearPasses = function(): void {
+        age += 1;
+    };
+    amIOld();
+    for (let i = 0; i < 3; i++) {
+        yearPasses();
+    }
+    amIOld();
+    console.log('');
+}
+
+function main() {
+    // Enter your code here
+    let t = parseInt(readLine());
+    for (let i = 0; i < t; i++) {
+        let person = Person(parseInt(readLine()));
+    }
+}
