277fa5b183219a30cd1ae4ac5c58078dcaa3f325
Inyong Hwang <inyong1020@gmail.com>
2021-08-17 20:39:28 +0900

    Create Q60_Insertion_Sort_list.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q60_Insertion_Sort_list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = parent = ListNode(0)
+        while head:
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+            cur.next, head.next, head = head, cur.next, head.next
+            if head and cur.val > head.val:
+                cur = parent
+        return parent.next
+        
