aa1e80338f6a5ff398b0218ad7b07f6d7bdd2c81
Inyong Hwang <inyong1020@gmail.com>
2021-08-18 15:35:20 +0900

    Create D05_Loops.ts

---
+++ V. Algorithm/ii. Site/A. Hacker Rank/30 Days of Code/TypeScript/D05_Loops.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+
+
+function main() {
+    const n: number = parseInt(readLine().trim(), 10);
+    for (let i = 1; i <= 10; i++) {
+        console.log(n + ' x ' + i + ' = ' + n*i );
+    }
+}
