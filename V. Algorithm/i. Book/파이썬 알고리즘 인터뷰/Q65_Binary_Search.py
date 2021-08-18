d3df35456d5c171eec1ab21b602d7d9ae44579a3
Inyong Hwang <inyong1020@gmail.com>
2021-08-18 19:14:54 +0900

    Update and rename Q65_binary_search.py to Q65_Binary_Search.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q65_Binary_Search.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q65_Binary_Search.py
@@ -1,3 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binary_search(left, right):
+            if left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] < target:
+                    return binary_search(mid + 1, right)
+                elif nums[mid] > target:
+                    return binary_search(left, mid - 1)
+                else:
+                    return mid
+            else:
+                return -1
+        return binary_search(0, len(nums) - 1)
+        
+
+'''
 def search(self, nums: List[int], target: int) -> int:
   def binary_search(left, right):
     if left <= right:
@@ -42,3 +59,4 @@ def search(self, nums: List[int], target: int) -> int:
     return nums.index(target)
   except ValueError:
     return -1
+'''
