e6db08deb60c8ce653a57824cf0f5cf7677aece8
Inyong Hwang <inyong1020@gmail.com>
2021-08-18 19:15:15 +0900

    Create 0704_Binary_Search.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/0704_Binary_Search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binary_search(left, right):
+            if left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] < target:
+                    return binary_search(mid + 1, right)
+                elif nums[mid] > target:
+                    return binary_search(left, mid - 1)
+                else:
+                    return mid
+            else:
+                return -1
+        return binary_search(0, len(nums) - 1)
+        
