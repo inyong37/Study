f7188ec76f31ca0e555de26f81fa885249e31970
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 09:34:33 +0900

    Create Q66_Search_in_Rotated_Sorted_Array.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q66_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        pivot = left
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_pivot = (mid + pivot) % len(nums)
+            if nums[mid_pivot] < target:
+                left = mid + 1
+            elif nums[mid_pivot] > target:
+                right = mid - 1
+            else:
+                return mid_pivot
+        return -1
+        
