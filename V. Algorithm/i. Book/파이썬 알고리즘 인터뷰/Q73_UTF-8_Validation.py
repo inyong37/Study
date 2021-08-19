f40c2ef5078c8ca4b091c96a1bb9eb70e4b44b8a
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 20:17:55 +0900

    Create Q73_UTF-8_Validation.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q73_UTF-8_Validation.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        def check(size):
+            for i in range(start + 1, start + size + 1):
+                if i >= len(data) or (data[i] >> 6) != 0b10:
+                    return False
+            return True
+        start = 0
+        while start < len(data):
+            first = data[start]
+            if (first >> 3) == 0b11110 and check(3):
+                start += 4
+            elif (first >> 4) == 0b1110 and check(2):
+                start += 3
+            elif (first >> 5) == 0b110 and check(1):
+                start += 2
+            elif (first >> 7) == 0:
+                start += 1
+            else:
+                return False
+        return True
+        
