7d2ac99aa405fff95c24d9fc3f37a90bebd28e69
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 21:29:00 +0900

    Create 0076_Minimum_Window_Substring.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/0076_Minimum_Window_Substring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)
+        missing = len(t)
+        left = start = end = 0
+        for right, char in enumerate(s, 1):
+            missing -= need[char] > 0
+            need[char] -= 1
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+                if not end or right - left < end - start:
+                    start, end = left, right
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+        return s[start:end]
+        
