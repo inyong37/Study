c9b1ca3b0b584810083371bb80f888b98fda45a3
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 20:54:05 +0900

    Create 0239_Sliding_Window_Maximum.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/0239_Sliding_Window_Maximum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        results = []
+        window = collections.deque()
+        current_max = float('-inf')
+        for i, v in enumerate(nums):
+            window.append(v)
+            if i < k - 1:
+                continue
+            if current_max == float('-inf'):
+                current_max = max(window)
+            elif v > current_max:
+                current_max = v
+            results.append(current_max)
+            if current_max == window.popleft():
+                current_max = float('-inf')
+        return results
+        
