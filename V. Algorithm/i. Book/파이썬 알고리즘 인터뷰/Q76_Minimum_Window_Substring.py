bb3ea643415c9e95c9880aa10461647fb8f8437f
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 21:29:29 +0900

    Update and rename Q76_minimum_window_substring.py to Q76_Minimum_Window_Substring.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q76_Minimum_Window_Substring.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q76_Minimum_Window_Substring.py
@@ -1,3 +1,23 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)
+        missing = len(t)
+        left = start = end = 0
+        for right, char in enumerate(s, 1):
+            missing -= need[char] > 0
+            need[char] -= 1
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+                if not end or right - left < end - start:
+                    start, end = left, right
+                need[s[left]] += 1
+                missing += 1
+                left += 1
+        return s[start:end]
+        
+'''
 def minWindow(self, s: str, t: str) -> str:
   def contains(s_substr_lst: List, t_lst: List):
     for t_elem in t_lst:
@@ -62,3 +82,4 @@ def minWindow(self, s:str, t: str) -> str:
       current_count[s[left]] -= 1
       left += 1
   return s[satrt: end] if end - start <= len(s) else ''
+'''
