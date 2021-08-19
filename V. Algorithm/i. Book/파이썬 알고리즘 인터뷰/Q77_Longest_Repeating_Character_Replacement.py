dec59573f1099ed9454c124b087fd2b908f2eaf8
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 21:39:32 +0900

    Create Q77_Longest_Repeating_Character_Replacement.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q77_Longest_Repeating_Character_Replacement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = right = 0
+        counts = collections.Counter()
+        for right in range(1, len(s) + 1):
+            counts[s[right - 1]] += 1
+            max_char_n = counts.most_common(1)[0][1]
+            if right - left - max_char_n > k:
+                counts[s[left]] -= 1
+                left += 1
+        return right - left
+        
