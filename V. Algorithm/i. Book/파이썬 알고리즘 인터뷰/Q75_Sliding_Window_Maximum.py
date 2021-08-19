6401ba1a3a0ec5a5f17c4a76696ce1c88e5b3fac
Inyong Hwang <inyong1020@gmail.com>
2021-08-19 20:54:44 +0900

    Update and rename Q75_sliding_window_maximum.py to Q75_Sliding_Window_Maximum.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q75_Sliding_Window_Maximum.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q75_Sliding_Window_Maximum.py
@@ -1,3 +1,23 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        results = []
+        window = collections.deque()
+        current_max = float('-inf')
+        for i, v in enumerate(nums):
+            window.append(v)
+            if i < k - 1:
+                continue
+            if current_max == float('-inf'):
+                current_max = max(window)
+            elif v > current_max:
+                current_max = v
+            results.append(current_max)
+            if current_max == window.popleft():
+                current_max = float('-inf')
+        return results
+        
+
+'''
 def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
   if not nums:
     return nums
@@ -32,3 +52,4 @@ def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
     if current_max == window.popleft():
       current_max = float('-inf')
 return result
+'''
