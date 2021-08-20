c358b0b7b0843d51ad9ceeb559b380fb44ce6282
Inyong Hwang <inyong1020@gmail.com>
2021-08-20 13:34:13 +0900

    Update and rename Q78_best_time_to_buy_and_sell_stock_ii.py to Q78_Best_Time_to_Buy_and_Sell_Stock_II.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q78_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q78_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -1,3 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum(max(prices[i+1] - prices[i], 0) for i in range(len(prices) - 1))
+        
+'''
 def maxProfit(self, prices: List[int]) -> int:
   result = 0
   for i in range(len(prices) - 1):
@@ -8,3 +13,4 @@ def maxProfit(self, prices: List[int]) -> int:
 
 def maxProfit(self, prices: List[int]) -> int:
   return sum(max(prices[i + 1] - prices[i], 0) for i in range(len(prices) - 1))
+'''
