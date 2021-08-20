5b6c4318a3f32c2fca593e247b12421e326ff03b
Inyong Hwang <inyong1020@gmail.com>
2021-08-20 10:27:05 +0900

    Create D07_Arrays.ts

---
+++ V. Algorithm/ii. Site/A. Hacker Rank/30 Days of Code/TypeScript/D07_Arrays.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+
+
+function main() {
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    let len: number = arr.length;
+    for (let i: number = 0; i < len; i++) {
+        let s: string = String(arr.pop());
+        process.stdout.write(s);
+        if (arr.length > 0) {
+            process.stdout.write(' ');
+        }
+    }
+}
