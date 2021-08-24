32f7f24d6e4cc622a265b32386f330ddb9f4107e
Inyong Hwang <inyong1020@gmail.com>
2021-08-24 17:59:59 +0900

    Create 0621_Task_Scheduler.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/0621_Task_Scheduler.py
@@ -0,0 +1,16 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counter = collections.Counter(tasks)
+        result = 0
+        while True:
+            sub_count = 0
+            for task, _ in counter.most_common(n + 1):
+                sub_count += 1
+                result += 1
+                counter.subtract(task)
+                counter += collections.Counter()
+            if not counter:
+                break
+            result += n - sub_count + 1
+        return result
+        
