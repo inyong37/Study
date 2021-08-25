df8c7cf7ae9cb5cc7dc9d000dae34526d7030cec
Inyong Hwang <inyong1020@gmail.com>
2021-08-25 13:19:03 +0900

    Create Q81_Gas_Station.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q81_Gas_Station.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        start, fuel = 0, 0
+        for i in range(len(gas)):
+            if gas[i] + fuel < cost[i]:
+                start = i + 1
+                fuel = 0
+            else:
+                fuel += gas[i] - cost[i]
+        return start
