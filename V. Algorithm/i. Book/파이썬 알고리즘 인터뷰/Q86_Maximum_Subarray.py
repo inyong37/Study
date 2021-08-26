ac4a05f3f93a7234fb43372c78dfaff8ed7f8ca0
Inyong Hwang <inyong1020@gmail.com>
2021-08-26 13:07:46 +0900

    Create Q86_Maximum_Subarray.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q86_Maximum_Subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        best_sum = -sys.maxsize
+        current_sum = 0
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            best_sum = max(best_sum, current_sum)
+        return best_sum
