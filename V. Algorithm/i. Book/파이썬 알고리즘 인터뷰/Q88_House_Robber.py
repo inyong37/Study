bf412327ca0d0574e67167ae7432439602037085
Inyong Hwang <inyong1020@gmail.com>
2021-08-26 17:35:39 +0900

    Create Q88_House_Robber.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q88_House_Robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) < 2:
+            return max(nums)
+        dp = collections.OrderedDict()
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp.popitem()[1]
+        
