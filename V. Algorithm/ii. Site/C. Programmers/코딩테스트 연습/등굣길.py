867bfe0b91f9d34eb0adedc15f688e13bc1acb1a
Inyong Hwang <inyong1020@gmail.com>
2021-10-01 21:19:21 +0900

    Create 등굣길.py

---
+++ V. Algorithm/ii. Site/C. Programmers/코딩테스트 연습/등굣길.py
@@ -0,0 +1,17 @@
+def solution(m, n, puddles):
+    table = [[1 if i == 1 else 0 for i in range(m + 1)] if j != 1 else [1 for _ in range(m + 1)] for j in range(n + 1)]
+    table[1][0] = 0
+    table[0][1] = 0
+    for [i, j] in puddles:
+        table[i][j] = -1
+    for i in range(2, n + 1):
+        for j in range(2, m + 1):
+            if table[i][j] == -1:
+                table[i][j] = 0
+            elif table[i][j-1] == -1:
+                table[i][j] = table[i-1][j]
+            elif table[i-1][j] == -1:
+                table[i][j] = table[i][j-1]
+            else:
+                table[i][j] = table[i-1][j] + table[i][j-1]
+    return table[n][m]
