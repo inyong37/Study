7f333b8946ed08f962a2d30cb4e5c831e455d100
Inyong Hwang <inyong1020@gmail.com>
2021-10-04 23:43:53 +0900

    Update Lesson10_Peaks.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson10_Peaks.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson10_Peaks.py
@@ -32,4 +32,32 @@ def solution(A):
                     break
         return i
     return 1
+
+# 2021-10-04-Mon, 100%, https://app.codility.com/demo/results/trainingGXF4JZ-2RS/
+
+def solution(A):
+    # check if the number of the A can make a peak.
+    if len(A) <= 2:
+        return 0
+    # check number of peaks.
+    peaks = []
+    for i in range(1, len(A)-1):
+        if A[i-1] < A[i] and A[i] > A[i+1]:
+            peaks.append(i)
+    # if the mountain is uphill or downhill
+    if len(peaks) == 0:
+        return 0
+    # if there is only one peak.
+    elif len(peaks) == 1:
+        return 1
     
+    for i in range(len(peaks), 0, -1):
+        if len(A) % i == 0:
+            k = len(A) // i
+            idx = 0
+            for peak in peaks:
+                if idx * k <= peak and peak < (idx+1) * k:
+                    idx += 1
+            if idx == i:
+                return i
+    return 1
