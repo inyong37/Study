1627bd542b1fbf3f11c5f07d50872f8ebd636e01
Inyong Hwang <inyong1020@gmail.com>
2021-10-29 17:59:00 +0900

    Update and rename 0787_Cheapest_Flight_Within_K_Stops.py to 0787_cheapest_flight_within_k_stops.py

---
+++ V. Algorithm/ii. Site/F. LeetCode/0787_cheapest_flight_within_k_stops.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        for u, v, w in flights:
+            graph[u].append((v, w))
+        Q = [(0, src, k)]
+        while Q:
+            price, node, k = heapq.heappop(Q)
+            if node == dst:
+                return price
+            if k >= 0:
+                for v, w in graph[node]:
+                    alt = price + w
+                    heapq.heappush(Q, (alt, v, k - 1))
+        return -1
+
+
+from typing import List
+from collections import defaultdict
+import heapq, sys
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = defaultdict(list)
+        weight = [(sys.maxsize, k)] * n
+        
+        for f, t, p in flights:
+            graph[f].append((t, p))
+        
+        Q = [(0, src, k)]
+        while Q:
+            price, node, k = heapq.heappop(Q)
+            if node == dst:
+                return price
+            if k >= 0:
+                for t, p in graph[node]:
+                    alt = price + p
+                    if alt < weight[t][0] or k-1 >= weight[t][1]:
+                        weight[t] = (alt, k-1)
+                        heapq.heappush(Q, (alt, t, k - 1))
+        return -1
