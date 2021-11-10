a68fcfaa194f0d11df4bed6817f82a1e02f1be1d
Inyong Hwang <inyong1020@gmail.com>
2021-11-10 14:16:15 +0900

    Update and rename Q66_Search_in_Rotated_Sorted_Array.py to Q66_search_in_rotated_sorted_array.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q66_search_in_rotated_sorted_array.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q66_search_in_rotated_sorted_array.py
@@ -2,6 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if not nums:
             return -1
+        
         left, right = 0, len(nums) - 1
         while left < right:
             mid = left + (right - left) // 2
@@ -10,6 +11,8 @@ class Solution:
             else:
                 right = mid
         pivot = left
+        # pivot = nums.index(min(nums))
+        
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + (right - left) // 2
