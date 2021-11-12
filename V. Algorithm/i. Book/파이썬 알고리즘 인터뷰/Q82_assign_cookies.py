1b10ba757e283155bc60503b0b2523ecb2a5e7a8
Inyong Hwang <inyong1020@gmail.com>
2021-11-12 16:51:10 +0900

    Update and rename Q82_Assign_Cookies.py to Q82_assign_cookies.py

---
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q82_assign_cookies.py
@@ -0,0 +1,27 @@
+from Typing import List
+
+class Solution1:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        child_i = cookie_j = 0
+        while child_i < len(g) and cookie_j < len(s):
+            if s[cookie_j] >= g[child_i]:
+                child_i += 1
+            cookie_j += 1
+        return child_i
+
+
+import bisect
+    
+    
+class Solution2:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        result = 0
+        for i in s:
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
+        return result
