324ec73f82980e905e843a545b292f3fa7136ded
Inyong Hwang <inyong1020@gmail.com>
2021-11-12 16:52:50 +0900

    Update and rename 0455_Assign_Cooikies.py to 0455_assign_cooikies.py

--- V. Algorithm/ii. Site/F. LeetCode/0455_assign_cooikies.py
+++ V. Algorithm/ii. Site/F. LeetCode/0455_assign_cooikies.py
@@ -1,3 +1,6 @@
+from Typing import List
+
+
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
@@ -9,3 +12,17 @@ class Solution:
             cookie_j += 1
         return child_i
         
+
+import bisect
+
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        result = 0
+        for i in s:
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
+        return result
