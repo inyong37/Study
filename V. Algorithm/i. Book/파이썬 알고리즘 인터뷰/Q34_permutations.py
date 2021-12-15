d662af66a4b7cffa88b2914ba3f71c6d12a8f4e3
Inyong Hwang <inyong1020@gmail.com>
2021-12-15 17:46:51 +0900

    Update and rename Q34_Permutations.py to Q34_permutations.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q34_permutations.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q34_permutations.py
@@ -1,4 +1,7 @@
+from typing import List
+import itertools
+
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         return list(itertools.permutations(nums))
-        
