68c366515237642a899b0b4e319082396a6a8839
Inyong Hwang <inyong1020@gmail.com>
2021-12-15 17:47:12 +0900

    Update and rename Q35_Combinations.py to Q35_combinations.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q35_combinations.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q35_combinations.py
@@ -1,3 +1,7 @@
+from typing import List
+import itertools
+
+
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         return list(itertools.combinations(range(1, n+1), k))
