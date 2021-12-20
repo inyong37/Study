0cc8862bbf4acec104cd4cad964219343fd4cde7
inyong37 <inyong1020@gmail.com>
2021-12-20 17:40:40 +0900

    upload fsl comment in py

---
+++ IV. COLAB/Enemy-Spotted/2. Uniform Classification/uniform_classification.py
@@ -0,0 +1,30 @@
+# Title: uniform classification: OCP(US) vs. EMR(RU) vs. XINGKONG(CN).
+# Author: Inyong Hwang
+# Date: 2021-12-20-Mon.
+# Few-shot learning based uniform pattern classification
+# Reference: https://www.kakaobrain.com/blog/106
+# Reference: https://www.kakaobrain.com/blog/112
+# Reference: https://blog.si-analytics.ai/3
+# Reference: https://blog.si-analytics.ai/7
+# Paper: https://github.com/Soloura/Study/tree/master/Computer%20Vision/Few-Shot%20Learning
+
+"""
+# Few-shot Learning
+### Approach: Data-driven vs. Model-based
+- Data-drive Approach: data generation with VAE and GAN
+
+### Model-based Approach: Metric Learning vs. Graph Neural Networks (GNN)
+- Metric Learning
+  - Siamese Neural Network for One-shot Image Recognition
+  - Matching Networks for One-shot learning
+  - Prototypical Networks for Few-shot Learning
+  - Learning to Compare: Relation Network for Few-shot Learning
+
+- Graph Neural Networks (GNN)
+  - Few-shot Learning with Graph Neural Networks
+  - Transductive Propagation Network for Few-shot Learning (TPN)
+  - Edge-Labeling Graph Neural Network for Few-shot Learning (EGNN)
+
+### Transfer Learning vs. Meta Learning (Episodic Learning, Learning-to-Learn)
+"""
+
