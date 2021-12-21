ef98bb16bdb2cbc170ca95ee33f888a6500ae5aa
Inyong Hwang <inyong1020@gmail.com>
2021-12-21 17:50:40 +0900

    Update Q07_two_sum.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q07_two_sum.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q07_two_sum.py
@@ -1,18 +1,23 @@
-class Solution:
+from typing import List
+
+
+class Solution1:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
-class Solution:
+
+class Solution2:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i, n in enumerate(nums):
             complement = target - n
             if complement in nums[i + 1:]:
                 return nums.index(n), nums[i + 1:].index(complement) + (i + 1)
 
-class Solution:
+
+class Solution3:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_map = {}
         for i, num in enumerate(nums):
@@ -21,12 +26,12 @@ class Solution:
         for i, num in enumerate(nums):
             if target - num in nums_map and i != nums_map[target - num]:
                 return nums.index(num), nums_map[target- num]
-            
-class Solution:
+
+
+class Solution4:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_map = {}
         for i, num in enumerate(nums):
             if target - num in nums_map:
                 return [nums_map[target - num], i]
             nums_map[num] = i
-
