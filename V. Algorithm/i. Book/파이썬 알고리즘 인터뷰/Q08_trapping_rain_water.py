49ba7881a6ee5d6fc0ff4708a2625d4f0b3f8b0a
Inyong Hwang <inyong1020@gmail.com>
2021-12-23 16:43:16 +0900

    Update Q08_trapping_rain_water.py

--- V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q08_trapping_rain_water.py
+++ V. Algorithm/i. Book/파이썬 알고리즘 인터뷰/Q08_trapping_rain_water.py
@@ -1,38 +1,42 @@
-# Solution 1
-def trap(self, height: List[int]) -> int:
-  if not height:
-    return 0
-  
-  volume = 0
-  left, right = 0, len(height) - 1
-  left_max, right_max = height[left], height[right]
-  
-  while left < right:
-    left_max, right_max = max(height[left], left_max), max(height[right], right_max)
-    
-    if left_max <= right_max:
-      volume += left_max - height[left]
-      left += 1
-    else:
-      volume += right_max - height[right]
-      right -= 1
-  return volume
+from typing import List
 
-# Solution 2
-def trap(self, heght: List[int]) -> int:
-  stack = []
-  volume = 0
-  
-  for i in range(len(height)):
-    while stack and height[i] > ehgith[stack[-1]]:
-      top = stack.pop()
-      
-      if not len(stack):
-        break
-      
-      distance = i - stack[-1] - 1
-      waters = min(height[i], height[stack[-1]]) - height[top]
-      
-      volume += distance * waters
-    stack.append()
-  return volume
+
+class Solution 1:
+  def trap(self, height: List[int]) -> int:
+    if not height:
+      return 0
+
+    volume = 0
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+
+    while left < right:
+      left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+
+      if left_max <= right_max:
+        volume += left_max - height[left]
+        left += 1
+      else:
+        volume += right_max - height[right]
+        right -= 1
+    return volume
+
+
+class Solution2:
+  def trap(self, heght: List[int]) -> int:
+    stack = []
+    volume = 0
+
+    for i in range(len(height)):
+      while stack and height[i] > ehgith[stack[-1]]:
+        top = stack.pop()
+
+        if not len(stack):
+          break
+
+        distance = i - stack[-1] - 1
+        waters = min(height[i], height[stack[-1]]) - height[top]
+
+        volume += distance * waters
+      stack.append()
+    return volume
