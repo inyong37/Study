27357cca7bd18f3b77393b0164ad922b210409c6
Inyong Hwang <inyong1020@gmail.com>
2022-07-12 18:01:13 +0900

    Update Lesson12_01_ChocolatesByNumbers.py

--- V. Algorithm/ii. Site/B. Codility/Lessons/Lesson12_01_ChocolatesByNumbers.py
+++ V. Algorithm/ii. Site/B. Codility/Lessons/Lesson12_01_ChocolatesByNumbers.py
@@ -18,3 +18,24 @@ def solution(N: int, M: int) -> int:
             target = l[pos]
         cnt += 1
     return cnt
+
+# 12% https://app.codility.com/demo/results/training8NMDB6-HZQ/
+
+def solution(N: int, M: int) -> int:
+    # write your code in Python 3.6
+    l = [1] * N
+    pos = 0
+    cnt = 0
+    target = l[pos]
+    while target == 1:
+        if pos + M < N - 1:
+            l[pos] = 0
+            pos = pos + M
+            target = l[pos]
+        else:
+        # if pos + M >= N:
+            l[pos] = 0
+            pos = pos + M - N
+            target = l[pos]
+        cnt += 1
+    return cnt
