4f90f66896b0c2875e2a6d34763da5ac8a378005
Inyong Hwang <inyong1020@gmail.com>
2023-10-18 10:00:33 +0900

    Update youtube.py

--- IX. ETC/G_Utility/youtube.py
+++ IX. ETC/G_Utility/youtube.py
@@ -6,7 +6,10 @@
 # Reference: https://pytube.io/en/latest/user/streams.html#downloading-streams
 
 
-def download_youtube_video(video_url: str='https://youtu.be/jNQXAC9IVRw?si=FyW2QN9hnnrEQeee', output_path: str='downloads/') -> None:
+def download_youtube_video(
+  video_url: str='https://youtu.be/jNQXAC9IVRw?si=FyW2QN9hnnrEQeee',
+  output_path: str='downloads/'
+) -> None:
   from pytube import YouTube
   video = YouTube(video_url)
   stream = video.streams.get_highest_resolution()
@@ -15,7 +18,10 @@ def download_youtube_video(video_url: str='https://youtu.be/jNQXAC9IVRw?si=FyW2Q
   return None
 
 
-def search_and_download_youtube_video(query: str='cat', output_path: str='downloads/') -> None:
+def search_and_download_youtube_video(
+  query: str='cat',
+  output_path: str='downloads/'
+) -> None:
   from pytube import YouTube
   search_results = YouTube(f'https://www.youtube.com/results?search_query={query}')
   video_url = f'https://www.youtube.com{search_results.find_all("a", {"class": "yt-uix-tile-link"})[0]["href"]}'
