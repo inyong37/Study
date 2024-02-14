ebbcedae27122aecd2bcb0bb2c1d278943068f04
Inyong Hwang <inyong1020@gmail.com>
2024-02-15 00:19:03 +0900

    Update T02_TorchVisionObjectDetectionFinetuning.py

--- _Framework/PyTorch/T02_TorchVisionObjectDetectionFinetuning.py
+++ _Framework/PyTorch/T02_TorchVisionObjectDetectionFinetuning.py
@@ -1,3 +1,224 @@
 # 2024-02-14-Wed.
 # https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 
+# wget https://www.cis.upenn.edu/~jshi/ped_html/PennFudanPed.zip -P data
+# cd data && unzip PennFudanPed.zip
+
+import matplotlib.pyplot as plt
+from torchvision.io import read_image
+
+image = read_image("data/PennFudanPed/PNGImages/FudanPed00046.png")
+mask = read_image("data/PennFudanPed/PedMasks/FudanPed00046_mask.png")
+
+plt.figure(figsize=(16, 8))
+plt.subplot(121)
+plt.title("Image")
+plt.imshow(image.permute(1, 2, 0))
+plt.subplot(122)
+plt.title("Mask")
+plt.imshow(mask.permute(1, 2, 0))
+
+import os
+import torch
+
+from torchvision.io import read_image
+from torchvision.ops.boxes import masks_to_boxes
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import functional as F
+
+
+class PennFudanDataset(torch.utils.data.Dataset):
+  def __init__(self, root, transforms):
+    self.root = root
+    self.transforms = transforms
+    self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+    self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+  
+  def __getitem__(self, idx):
+    img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+    masks_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+    img = read_images(img_path)
+    mask = read_image(mask_path)
+    obj_ids = torch.unique(mask)
+    obj_ids = obj_ids[1:]
+    num_objs = len(obj_idx)
+    masks = (mask == obj_idx[:, None, None]).to(dtype=torch.uint8)
+    boxes = masks_to_boxes(masks)
+    labels = torch.ones((num_objs,), dtype=torch.int64)
+    image_id = idx
+    area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+    iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+    img = tv_tensors.Image(img)
+    target = {}
+    target["boxes"] = tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img))
+    target["masks"] = tv_tensors.Mask(masks)
+    target["labels"] = labels
+    target["image_id"] = image_id
+    target["area"] = area
+    target["iscrowd"] = iscrowd
+    if self.transforms is not None:
+        img, target = self.transforms(img, target)
+    return img, target
+  def __len__(self):
+      return len(self.imgs)
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+num_classes = 2
+in_features = model.roi_heads.box_predictor.cls_score.in_features
+model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+backbone = torchvision.models.mobilenet_v2(weights="DEFAULT").features
+backbone.out_channels = 1280
+anchor_generator = AnchorGenerator(
+  sizes=((32, 64, 128, 256, 512),),
+  aspect_ratios=((0.5, 1.0, 2.0),)
+)
+
+roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+  featmap_names=['0'],
+  output_size=7,
+  sampling_ratio=2
+)
+
+model = FasterRCNN(
+  backbone,
+  num_classes=2,
+  rpn_anchor_generator_anchor_generator,
+  box_roi_pool=roi_pooler
+)
+
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+def get_model_instance_segmentation(num_classes):
+  model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+  in_features = model.roi_heads.box_predictor.cls_score.in_features
+  model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+  in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+  hidden_layer = 256
+  model.roi_heads.mask_predictor = MaskRCNNPredictor(
+    in_features_mask,
+    hidden_layer,
+    num_classes
+  )
+  return model
+
+os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/engine.py")
+os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/utils.py")
+os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/coco_utils.py")
+os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/coco_eval.py")
+os.system("wget https://raw.githubusercontent.com/pytorch/vision/main/references/detection/transforms.py")
+
+from torchvision.transforms import v2 as T
+
+
+def get_transform(train):
+  transforms = []
+  if train:
+    transforms.append(T.RandomHorizontalFlip(0.5))
+  transforms.append(T.ToDtype(torch.float, scale=True))
+  transforms.append(T.ToPureTensor())
+  return T.Compose(transforms)
+
+
+import utils
+
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+dataset = PennFudanDataset('data/PennFudanPed', get_transform(train=True))
+data_loader = torch.utils.data.DataLoader(
+  dataset,
+  batch_size=2,
+  shuffle=True,
+  num_workers=4,
+  collate_fn=utils.collate_fn
+)
+
+images, targets = next(iter(data_loader))
+images = list(image for image in images)
+targets = [{k: v for k, v in t.items()} for t in targets]
+output = model(images, targets)
+print(output)
+
+model.eval()
+x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+predictions = model(x)
+print(predictions[0])
+
+from engine import train_one_epoch, evaluate
+
+device = torch.device('cuda') if torch.cuda.is_available() elase torch.device('cpu')
+
+num_classes = 2
+dataset = PennFudanDataset('data/PennFudanPed', get_transform(train=True))
+dataset_test = PennFudanDataset('data/PennFudanPen', get_transform(train=False))
+indices = torch.randperm(len(dataset)).tolist()
+dataset = torch.utils.data.Subset(dataset, indices[:-50])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+data_loader = torch.utils.data.DataLoader(
+  dataset,
+  batch_size=2,
+  shuffle=True,
+  num_workers=4,
+  collate_fn=utils.collate_fn
+)
+
+data_loader_test = torch.utils.data.DataLoader(
+  dataset_test,
+  batch_size=1,
+  shuffle=False,
+  num_workers=4,
+  collate_fn=utils.collate_fn
+)
+
+model = get_model_instance_segmentation(num_classes)
+model.to(device)
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(
+  params,
+  lr=0.005,
+  momentum=0.9,
+  weight_decay=0.0005
+)
+
+num_epochs=2
+
+for epoch in range(num_epochs):
+  train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+  lr_scheduler.step()
+  evaluate(model, data_loader_test, device=device)
+
+print("That's it!")
+
+
+import matplotlib.pyplot as plt
+
+from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
+
+image = read_image("data/PennFudanPed/PNGImages/FudanPed00046.png")
+eval_transform = get_transform(train=False)
+
+model.eval()
+with torch.no_grad():
+  x = eval_transform(image)
+  x = x[:3, ...].to(device)
+  predictions = model([x, ])
+  pred = predictions[0]
+
+image = (255.0 * (image - image.min()) / (image.max() - image.min())).to(torch.uint8)
+image = image[:3, ...]
+pred_labels = [f"pedestrian: {score:.3f}" for label, score in zip(pred["labels"], pred["scores"])]
+pred_boxes = pred["boxes"].long()
+output_image = draw_bounding_boxes(image, pred_boxes, pred_labels, colors="red")
+masks = (pred["masks"] > 0.7).squeeze(1)
+output_image = draw_segmentation_masks(output_image, masks, alpha=0.5, colors="blue")
+plt.figure(figsize=(12, 12))
+plt.imshow(output_image.permute(1, 2, 0))
