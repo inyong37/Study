c9e6797d101f39d541c7df5f2871f0655ea82bdc
Inyong Hwang <inyong1020@gmail.com>
2024-02-14 20:03:17 +0900

    Create T01_RunInferenceWithPipelines.py

---
+++ _Framework/HuggingFace/Transformers/T01_RunInferenceWithPipelines.py
@@ -0,0 +1,113 @@
+# 2024-02-14-Wed.
+# https://huggingface.co/docs/transformers/en/pipeline_tutorial
+
+from transformers import pipeline
+
+transcriber = pipeline(task="automatic-speech-recognition")
+transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
+# {'text': 'I HAVE A DREAM BUT ONE DAY THIS NATION WILL RISE UP LIVE UP THE TRUE MEANING OF ITS TREES'}
+
+transcriber = pipeline(model="openai/whisper-large-v2")
+transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
+# {'text': ' I have a dream that one day this nation will rise up and live out the true meaning of its creed.'}
+
+transcriber(
+    [
+        "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac",
+        "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/1.flac",
+    ]
+)
+
+transcriber = pipeline(model="openai/whisper-large-v2", my_parameter=1)
+
+out = transcriber(...) 
+out = transcriber(..., my_parameter=2)
+out = transcriber(...)
+
+transcriber = pipeline(model="openai/whisper-large-v2", device=0)
+
+transcriber = pipeline(model="openai/whisper-large-v2", device_map="auto")
+
+transcriber = pipeline(model="openai/whisper-large-v2", device=0, batch_size=2)
+audio_filenames = [f"https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/{i}.flac" for i in range(1, 5)]
+texts = transcriber(audio_filenames)
+
+transcriber = pipeline(model="openai/whisper-large-v2", return_timestamps=True)
+transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
+# {'text': ' I have a dream that one day this nation will rise up and live out the true meaning of its creed.', 'chunks': [{'timestamp': (0.0, 11.88), 'text': ' I have a dream that one day this nation will rise up and live out the true meaning of its'}, {'timestamp': (11.88, 12.38), 'text': ' creed.'}]}
+
+transcriber = pipeline(model="openai/whisper-large-v2", chunk_length_s=30, return_timestamps=True)
+transcriber("https://huggingface.co/datasets/sanchit-gandhi/librispeech_long/resolve/main/audio.wav")
+# {'text': " Chapter 16. I might have told you of the beginning of this liaison in a few lines, but I wanted you to see every step by which we came.  I, too, agree to whatever Marguerite wished, Marguerite to be unable to live apart from me. It was the day after the evening...
+
+def data():
+    for i in range(1000):
+        yield f"My example {i}"
+
+
+pipe = pipeline(model="gpt2", device=0)
+generated_characters = 0
+for out in pipe(data()):
+    generated_characters += len(out[0]["generated_text"])
+
+
+from transformers.pipelines.pt_utils import KeyDataset
+from datasets import load_dataset
+
+pipe = pipeline(model="hf-internal-testing/tiny-random-wav2vec2", device=0)
+dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation[:10]")
+
+for out in pipe(KeyDataset(dataset, "audio")):
+    print(out)
+
+# Vision
+
+from transformers import pipeline
+
+vision_classifier = pipeline(model="google/vit-base-patch16-224")
+preds = vision_classifier(
+    images="https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/pipeline-cat-chonk.jpeg"
+)
+preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in preds]
+print(preds)
+# [{'score': 0.4335, 'label': 'lynx, catamount'}, {'score': 0.0348, 'label': 'cougar, puma, catamount, mountain lion, painter, panther, Felis concolor'}, {'score': 0.0324, 'label': 'snow leopard, ounce, Panthera uncia'}, {'score': 0.0239, 'label': 'Egyptian cat'}, {'score': 0.0229, 'label': 'tiger cat'}]
+
+# Text
+
+from transformers import pipeline
+
+# This model is a `zero-shot-classification` model.
+# It will classify text, except you are free to choose any label you might imagine
+classifier = pipeline(model="facebook/bart-large-mnli")
+classifier(
+    "I have a problem with my iphone that needs to be resolved asap!!",
+    candidate_labels=["urgent", "not urgent", "phone", "tablet", "computer"],
+)
+# {'sequence': 'I have a problem with my iphone that needs to be resolved asap!!', 'labels': ['urgent', 'phone', 'computer', 'not urgent', 'tablet'], 'scores': [0.504, 0.479, 0.013, 0.003, 0.002]}
+
+# Multimodal
+
+from transformers import pipeline
+
+vqa = pipeline(model="impira/layoutlm-document-qa")
+vqa(
+    image="https://huggingface.co/spaces/impira/docquery/resolve/2359223c1837a7587402bda0f2643382a6eefeab/invoice.png",
+    question="What is the invoice number?",
+)
+# [{'score': 0.42515, 'answer': 'us-001', 'start': 16, 'end': 16}]
+
+# Accelerate
+
+# pip install accelerate
+import torch
+from transformers import pipeline
+
+pipe = pipeline(model="facebook/opt-1.3b", torch_dtype=torch.bfloat16, device_map="auto")
+output = pipe("This is a cool example!", do_sample=True, top_p=0.95)
+
+# pip install accelerate bitsandbytes
+import torch
+from transformers import pipeline
+
+pipe = pipeline(model="facebook/opt-1.3b", device_map="auto", model_kwargs={"load_in_8bit": True})
+output = pipe("This is a cool example!", do_sample=True, top_p=0.95)
