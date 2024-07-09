3273a0783f782e135fdc4e6c0305ef4ea57d7c03
Inyong Hwang <inyong1020@gmail.com>
2024-07-09 21:54:17 +0900

    Create test_example_pytest.py

---
+++ VI. Language/iii. Python/test_example_pytest.py
@@ -0,0 +1,15 @@
+import pytest
+
+def setup_function():
+  # Code to set up test fixtures, if any
+  pass
+
+def teardown_function():
+  # Code to clean up after tests
+  pass
+
+def test_addition():
+  assert 1 + 1 == 2
+
+def test_subtraction():
+  assert 2 - 1 == 1
