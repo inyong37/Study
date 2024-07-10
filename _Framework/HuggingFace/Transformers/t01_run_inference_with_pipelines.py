d2f429e0a1592312a0bacb29484cd5712135f590
Inyong Hwang <inyong1020@gmail.com>
2024-07-10 21:23:15 +0900

    Update and rename run_inference_with_pipelines.py to t01_run_inference_with_pipelines.py

--- _Framework/HuggingFace/Transformers/t01_run_inference_with_pipelines.py
+++ _Framework/HuggingFace/Transformers/t01_run_inference_with_pipelines.py
@@ -1,5 +1,10 @@
 # 2024-02-14-Wed.
+# 2024-07-10-Wed.
 # https://huggingface.co/docs/transformers/en/pipeline_tutorial
+
+# Pipelines for inference
+
+# Pipeline usage
 
 from transformers import pipeline
 
@@ -18,19 +23,27 @@ transcriber(
     ]
 )
 
+# Parameters
+
 transcriber = pipeline(model="openai/whisper-large-v2", my_parameter=1)
 
-out = transcriber(...) 
-out = transcriber(..., my_parameter=2)
-out = transcriber(...)
+out = transcriber(...)  # This will use `my_parameter=1`.
+out = transcriber(..., my_parameter=2)  # This will override and use `my_parameter=2`.
+out = transcriber(...)  # This will go back to using `my_parameter=1`.
+
+# Device
 
 transcriber = pipeline(model="openai/whisper-large-v2", device=0)
 
-transcriber = pipeline(model="openai/whisper-large-v2", device_map="auto")
+transcriber = pipeline(model="openai/whisper-large-v2", device_map="auto") # pip install --upgrade accelerate
+
+# Batch size
 
 transcriber = pipeline(model="openai/whisper-large-v2", device=0, batch_size=2)
 audio_filenames = [f"https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/{i}.flac" for i in range(1, 5)]
 texts = transcriber(audio_filenames)
+
+# Task specific parameters
 
 transcriber = pipeline(model="openai/whisper-large-v2", return_timestamps=True)
 transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
@@ -39,6 +52,8 @@ transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.f
 transcriber = pipeline(model="openai/whisper-large-v2", chunk_length_s=30, return_timestamps=True)
 transcriber("https://huggingface.co/datasets/sanchit-gandhi/librispeech_long/resolve/main/audio.wav")
 # {'text': " Chapter 16. I might have told you of the beginning of this liaison in a few lines, but I wanted you to see every step by which we came.  I, too, agree to whatever Marguerite wished, Marguerite to be unable to live apart from me. It was the day after the evening...
+
+# Using pipelines on a dataset
 
 def data():
     for i in range(1000):
@@ -50,7 +65,7 @@ generated_characters = 0
 for out in pipe(data()):
     generated_characters += len(out[0]["generated_text"])
 
-
+# KeyDataset is a util that will just output the item we're interested in.
 from transformers.pipelines.pt_utils import KeyDataset
 from datasets import load_dataset
 
@@ -60,7 +75,7 @@ dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", spl
 for out in pipe(KeyDataset(dataset, "audio")):
     print(out)
 
-# Vision
+# Vision pipeline
 
 from transformers import pipeline
 
@@ -72,7 +87,7 @@ preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in 
 print(preds)
 # [{'score': 0.4335, 'label': 'lynx, catamount'}, {'score': 0.0348, 'label': 'cougar, puma, catamount, mountain lion, painter, panther, Felis concolor'}, {'score': 0.0324, 'label': 'snow leopard, ounce, Panthera uncia'}, {'score': 0.0239, 'label': 'Egyptian cat'}, {'score': 0.0229, 'label': 'tiger cat'}]
 
-# Text
+# Text pipeline
 
 from transformers import pipeline
 
@@ -85,7 +100,7 @@ classifier(
 )
 # {'sequence': 'I have a problem with my iphone that needs to be resolved asap!!', 'labels': ['urgent', 'phone', 'computer', 'not urgent', 'tablet'], 'scores': [0.504, 0.479, 0.013, 0.003, 0.002]}
 
-# Multimodal
+# Multimodal pipeline
 
 from transformers import pipeline
 
@@ -94,9 +109,11 @@ vqa(
     image="https://huggingface.co/spaces/impira/docquery/resolve/2359223c1837a7587402bda0f2643382a6eefeab/invoice.png",
     question="What is the invoice number?",
 )
+output[0]["score"] = round(output[0]["score"], 3)
+print(output)
 # [{'score': 0.42515, 'answer': 'us-001', 'start': 16, 'end': 16}]
 
-# Accelerate
+# Using pipeline on large models with accelerate
 
 # pip install accelerate
 import torch
@@ -111,3 +128,12 @@ from transformers import pipeline
 
 pipe = pipeline(model="facebook/opt-1.3b", device_map="auto", model_kwargs={"load_in_8bit": True})
 output = pipe("This is a cool example!", do_sample=True, top_p=0.95)
+
+# Creating web demos from pipelines with gradio
+
+from transformers import pipeline
+import gradio as gr # pip install gradio
+
+pipe = pipeline("image-classification", model="google/vit-base-patch16-224")
+
+gr.Interface.from_pipeline(pipe).launch()
